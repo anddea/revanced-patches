231b2dab73981ba877f5f8b394a926ba3c179446
inotia00 <ars258000@gmail.com>
2023-01-05 19:05:01 +0900

    remove `add-missing-transition-patch`

--- src/main/kotlin/app/revanced/patches/youtube/misc/optimize/patch/OptimizeResourcePatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/optimize/patch/OptimizeResourcePatch.kt
@@ -9,7 +9,6 @@ import app.revanced.patcher.patch.PatchResultSuccess
 import app.revanced.patcher.patch.ResourcePatch
 import app.revanced.patcher.patch.annotations.DependsOn
 import app.revanced.patcher.patch.annotations.Patch
-import app.revanced.patches.youtube.misc.optimize.patch.MissingTranslationPatch
 import app.revanced.patches.youtube.misc.optimize.patch.RedundantResourcePatch
 import app.revanced.patches.youtube.misc.settings.resource.patch.SettingsPatch
 import app.revanced.shared.annotation.YouTubeCompatibility
@@ -20,7 +19,6 @@ import app.revanced.shared.util.resources.ResourceHelper
 @DependsOn(
     [
         RedundantResourcePatch::class,
-        MissingTranslationPatch::class,
         SettingsPatch::class
     ]
 )
