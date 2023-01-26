f18d64cfb2555747ed9fbfba87a55788da20999b
inotia00 <ars258000@gmail.com>
2023-01-27 01:05:14 +0900

    add `hide-snackbar` patch

---
+++ src/main/kotlin/app/revanced/patches/youtube/layout/general/snackbar/resource/patch/HideSnackbarPatch.kt
@@ -0,0 +1,49 @@
+package app.revanced.patches.youtube.layout.general.snackbar.resource.patch
+
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.ResourceContext
+import app.revanced.patcher.patch.annotations.DependsOn
+import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.ResourcePatch
+import app.revanced.patches.youtube.layout.general.snackbar.bytecode.patch.HideSnackbarBytecodePatch
+import app.revanced.patches.youtube.misc.settings.resource.patch.SettingsPatch
+import app.revanced.shared.annotation.YouTubeCompatibility
+import app.revanced.shared.util.resources.ResourceHelper
+
+@Patch
+@Name("hide-snackbar")
+@Description("Hides the snackbar action popup.")
+@DependsOn(
+    [
+        HideSnackbarBytecodePatch::class,
+        SettingsPatch::class
+    ]
+)
+@YouTubeCompatibility
+@Version("0.0.1")
+class HideSnackbarPatch : ResourcePatch {
+    override fun execute(context: ResourceContext): PatchResult {
+
+        /*
+         add settings
+         */
+        ResourceHelper.addSettings2(
+            context,
+            "PREFERENCE_CATEGORY: REVANCED_SETTINGS",
+            "PREFERENCE: LAYOUT_SETTINGS",
+            "PREFERENCE_HEADER: GENERAL",
+            "SETTINGS: HIDE_SNACKBAR"
+        )
+
+        ResourceHelper.patchSuccess(
+            context,
+            "hide-snackbar"
+        )
+
+        return PatchResultSuccess()
+    }
+}
