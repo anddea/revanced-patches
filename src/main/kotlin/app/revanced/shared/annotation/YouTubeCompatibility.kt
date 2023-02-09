4bee05e495277bef28446a60cc7271e9ef6e5d7b
inotia00 <ars258000@gmail.com>
2023-02-09 10:25:00 +0900

    add suport YouTube v18.04.43, v18.05.35

--- src/main/kotlin/app/revanced/shared/annotation/YouTubeCompatibility.kt
+++ src/main/kotlin/app/revanced/shared/annotation/YouTubeCompatibility.kt
@@ -5,7 +5,7 @@ import app.revanced.patcher.annotation.Package
 
 @Compatibility(
     [Package(
-        "com.google.android.youtube", arrayOf("17.49.37", "18.01.38", "18.02.37", "18.03.42", "18.04.41", "18.05.34")
+        "com.google.android.youtube", arrayOf("18.04.43", "18.05.35")
     )]
 )
 @Target(AnnotationTarget.CLASS)
