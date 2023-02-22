07c5f9232143bab927030cd872bb93eea12451c2
inotia00 <ars258000@gmail.com>
2023-02-22 23:36:41 +0900

    add `hide-seekbar` patch

---
+++ src/main/kotlin/app/revanced/patches/youtube/layout/seekbar/seekbar/patch/HideSeekbarPatch.kt
@@ -0,0 +1,60 @@
+package app.revanced.patches.youtube.layout.seekbar.seekbar.patch
+
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.addInstructions
+import app.revanced.patcher.extensions.instruction
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.annotations.DependsOn
+import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patcher.util.smali.ExternalLabel
+import app.revanced.patches.shared.annotation.YouTubeCompatibility
+import app.revanced.patches.youtube.misc.settings.resource.patch.SettingsPatch
+import app.revanced.patches.youtube.misc.timebar.patch.HookTimebarPatch
+import app.revanced.util.integrations.Constants.SEEKBAR_LAYOUT
+
+@Patch
+@Name("hide-seekbar")
+@Description("Hides the seekbar.")
+@DependsOn(
+    [
+        HookTimebarPatch::class,
+        SettingsPatch::class
+    ]
+)
+@YouTubeCompatibility
+@Version("0.0.1")
+class HideSeekbarPatch : BytecodePatch() {
+    override fun execute(context: BytecodeContext): PatchResult {
+
+        val insertMethod = HookTimebarPatch.setTimebarMethod
+
+        insertMethod.addInstructions(
+            0, """
+                invoke-static {}, $SEEKBAR_LAYOUT->hideSeekbar()Z
+                move-result v0
+                if-eqz v0, :show_seekbar
+                return-void
+                """, listOf(ExternalLabel("show_seekbar", insertMethod.instruction(0)))
+        )
+
+        /*
+         * Add settings
+         */
+        SettingsPatch.addPreference(
+            arrayOf(
+                "PREFERENCE: OTHER_LAYOUT_SETTINGS",
+                "PREFERENCE_HEADER: SEEKBAR",
+                "SETTINGS: HIDE_SEEKBAR"
+            )
+        )
+
+        SettingsPatch.updatePatchStatus("hide-seekbar")
+
+        return PatchResultSuccess()
+    }
+}
