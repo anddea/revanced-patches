ee22106c8cce98b286fe6addffabc241c41964bd
inotia00 <ars258000@gmail.com>
2023-03-09 09:36:58 +0900

    add support YouTube v18.09.37

--- src/main/kotlin/app/revanced/patches/youtube/misc/returnyoutubedislike/fingerprints/ShortsTextComponentParentFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/returnyoutubedislike/fingerprints/ShortsTextComponentParentFingerprint.kt
@@ -2,12 +2,10 @@ package app.revanced.patches.youtube.misc.returnyoutubedislike.fingerprints
 
 import app.revanced.patcher.extensions.or
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
-import org.jf.dexlib2.AccessFlags
 import org.jf.dexlib2.Opcode
 
 object ShortsTextComponentParentFingerprint : MethodFingerprint(
     returnType = "V",
-    access = AccessFlags.PROTECTED or AccessFlags.FINAL,
     parameters = listOf("L", "L"),
     opcodes = listOf(
         Opcode.INVOKE_STATIC,
