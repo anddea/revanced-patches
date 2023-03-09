d22c96fbffbb49ddeec0c4246d2802866b9a263e
inotia00 <ars258000@gmail.com>
2023-03-09 09:34:27 +0900

    add `hide-music-button` patch

---
+++ src/main/kotlin/app/revanced/patches/youtube/layout/player/musicbutton/patch/HideMusicButtonPatch.kt
@@ -0,0 +1,44 @@
+package app.revanced.patches.youtube.layout.player.musicbutton.patch
+
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.ResourceContext
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.ResourcePatch
+import app.revanced.patcher.patch.annotations.DependsOn
+import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patches.shared.annotation.YouTubeCompatibility
+import app.revanced.patches.youtube.misc.playerbutton.patch.PlayerButtonPatch
+import app.revanced.patches.youtube.misc.settings.resource.patch.SettingsPatch
+
+@Patch
+@Name("hide-music-button")
+@Description("Hides the YouTube Music button in the video player.")
+@DependsOn(
+    [
+        PlayerButtonPatch::class,
+        SettingsPatch::class
+    ]
+)
+@YouTubeCompatibility
+@Version("0.0.1")
+class HideMusicButtonPatch : ResourcePatch {
+    override fun execute(context: ResourceContext): PatchResult {
+
+        /*
+         * Add settings
+         */
+        SettingsPatch.addPreference(
+            arrayOf(
+                "PREFERENCE: PLAYER_LAYOUT_SETTINGS",
+                "SETTINGS: HIDE_YOUTUBE_MUSIC_BUTTON"
+            )
+        )
+
+        SettingsPatch.updatePatchStatus("hide-music-button")
+
+        return PatchResultSuccess()
+    }
+}
