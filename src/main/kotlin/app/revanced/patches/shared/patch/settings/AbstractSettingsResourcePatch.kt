7d4d34a206be2f9f6c88c7f6b1a9121935389b06
inotia00 <ars258000@gmail.com>
2023-03-11 07:53:48 +0900

    remove deprecated permission

--- src/main/kotlin/app/revanced/patches/shared/patch/settings/AbstractSettingsResourcePatch.kt
+++ src/main/kotlin/app/revanced/patches/shared/patch/settings/AbstractSettingsResourcePatch.kt
@@ -4,7 +4,6 @@ import app.revanced.patcher.data.ResourceContext
 import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.PatchResultSuccess
 import app.revanced.patcher.patch.ResourcePatch
-import app.revanced.util.resources.ResourceHelper
 import app.revanced.util.resources.ResourceUtils
 import app.revanced.util.resources.ResourceUtils.copyResources
 import app.revanced.util.resources.ResourceUtils.copyXmlNode
@@ -13,34 +12,22 @@ import app.revanced.util.resources.ResourceUtils.copyXmlNode
  * Abstract settings resource patch
  *
  * @param sourceDirectory Source directory to copy the preference template from
- * @param sourcehostDirectory Source directory to copy the preference template from
+ * @param sourceHostDirectory Source directory to copy the preference template from
  */
 abstract class AbstractSettingsResourcePatch(
     private val sourceDirectory: String,
-    private val sourcehostDirectory: String,
+    private val sourceHostDirectory: String,
     private val isYouTube: Boolean,
 ) : ResourcePatch {
     override fun execute(context: ResourceContext): PatchResult {
         /*
          * Copy strings
          */
-        context.copyXmlNode(sourcehostDirectory, "values/strings.xml", "resources")
+        context.copyXmlNode(sourceHostDirectory, "values/strings.xml", "resources")
 
         /* initialize ReVanced Settings */
-        if (isYouTube) {
-            /*
-             * used for self-restart
-             */
-            context.xmlEditor["AndroidManifest.xml"].use { editor ->
-                editor.file.getElementsByTagName("manifest").item(0).also {
-                    it.appendChild(it.ownerDocument.createElement("uses-permission").also { element ->
-                        element.setAttribute("android:name", "android.permission.SCHEDULE_EXACT_ALARM")
-                    })
-                }
-            }
-
+        if (isYouTube)
             context.copyResources(sourceDirectory, ResourceUtils.ResourceGroup("xml", "revanced_prefs.xml"))
-        }
 
         return PatchResultSuccess()
     }
