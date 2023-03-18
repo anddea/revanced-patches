f8fcf36a2cd220d81e45f1fdcad3ca21920274e7
inotia00 <ars258000@gmail.com>
2023-03-18 22:21:28 +0900

    refactor(double-back-to-close): use better fingerprints

--- src/main/kotlin/app/revanced/patches/youtube/ads/doublebacktoclose/fingerprint/ScrollTopParentFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/ads/doublebacktoclose/fingerprint/ScrollTopParentFingerprint.kt
@@ -8,19 +8,18 @@ import org.jf.dexlib2.Opcode
 object ScrollTopParentFingerprint : MethodFingerprint(
     returnType = "V",
     access = AccessFlags.PUBLIC or AccessFlags.CONSTRUCTOR,
-    parameters = listOf("L", "L", "L", "L"),
     opcodes = listOf(
-        Opcode.INVOKE_DIRECT,
         Opcode.IPUT_OBJECT,
         Opcode.IPUT_OBJECT,
         Opcode.IPUT_OBJECT,
         Opcode.IPUT_OBJECT,
         Opcode.CONST_16,
         Opcode.INVOKE_VIRTUAL,
-        Opcode.NEW_INSTANCE
+        Opcode.NEW_INSTANCE,
+        Opcode.INVOKE_DIRECT,
+        Opcode.IPUT_OBJECT,
+        Opcode.RETURN_VOID
     ),
-    customFingerprint = { methodDef ->
-        methodDef.name == "<init>"
-    }
+    customFingerprint = { it.name == "<init>"}
 )
 
