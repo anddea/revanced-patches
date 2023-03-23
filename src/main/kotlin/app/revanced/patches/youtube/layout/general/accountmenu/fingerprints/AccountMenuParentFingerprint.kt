f8c8b01b2662e92bb5b483ecb7dba81bccec82c6
inotia00 <ars258000@gmail.com>
2023-03-23 23:10:03 +0900

    add `hide-account-menu` patch

---
+++ src/main/kotlin/app/revanced/patches/youtube/layout/general/accountmenu/fingerprints/AccountMenuParentFingerprint.kt
@@ -0,0 +1,21 @@
+package app.revanced.patches.youtube.layout.general.accountmenu.fingerprints
+
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import app.revanced.patches.youtube.misc.resourceid.patch.SharedResourceIdPatch
+import org.jf.dexlib2.Opcode
+import org.jf.dexlib2.iface.instruction.WideLiteralInstruction
+
+object AccountMenuParentFingerprint : MethodFingerprint(
+    opcodes = listOf(
+        Opcode.CONST,
+        Opcode.CONST_4,
+        Opcode.INVOKE_VIRTUAL,
+        Opcode.MOVE_RESULT_OBJECT
+    ),
+    customFingerprint = { methodDef ->
+        methodDef.implementation?.instructions?.any {
+            it.opcode.ordinal == Opcode.CONST.ordinal &&
+            (it as? WideLiteralInstruction)?.wideLiteral == SharedResourceIdPatch.compactLinkLabelId
+        } == true
+    }
+)
