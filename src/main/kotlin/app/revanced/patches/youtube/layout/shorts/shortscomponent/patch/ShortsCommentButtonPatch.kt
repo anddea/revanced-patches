f56d1b35c0d1f55407c457c2169eb3af2f792115
inotia00 <ars258000@gmail.com>
2023-04-08 07:07:03 +0900

    fix typo

--- src/main/kotlin/app/revanced/patches/youtube/layout/shorts/shortscomponent/patch/ShortsCommentButtonPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/shorts/shortscomponent/patch/ShortsCommentButtonPatch.kt
@@ -12,7 +12,7 @@ import app.revanced.patcher.patch.annotations.DependsOn
 import app.revanced.patches.shared.annotation.YouTubeCompatibility
 import app.revanced.patches.youtube.layout.shorts.shortscomponent.fingerprints.ShortsCommentFingerprint
 import app.revanced.patches.youtube.misc.resourceid.patch.SharedResourceIdPatch
-import app.revanced.util.integrations.Constants.GENERAL
+import app.revanced.util.integrations.Constants.SHORTS
 import org.jf.dexlib2.iface.instruction.OneRegisterInstruction
 import org.jf.dexlib2.iface.instruction.WideLiteralInstruction
 
@@ -34,7 +34,7 @@ class ShortsCommentButtonPatch : BytecodePatch(
 
                 method.addInstruction(
                     insertIndex + 1,
-                    "invoke-static {v$insertRegister}, $GENERAL->hideShortsPlayerCommentsButton(Landroid/view/View;)V"
+                    "invoke-static {v$insertRegister}, $SHORTS->hideShortsPlayerCommentsButton(Landroid/view/View;)V"
                 )
             }
         } ?: return ShortsCommentFingerprint.toErrorResult()
