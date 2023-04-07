4321b573514bca830d21baab5bdae7c2659071e6
inotia00 <ars258000@gmail.com>
2023-04-08 07:46:52 +0900

    add `change-homepage` patch

---
+++ src/main/kotlin/app/revanced/patches/youtube/layout/navigation/homepage/patch/ChangeHomePagePatch.kt
@@ -0,0 +1,73 @@
+package app.revanced.patches.youtube.layout.navigation.homepage.patch
+
+import app.revanced.extensions.toErrorResult
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.addInstructions
+import app.revanced.patcher.extensions.instruction
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.annotations.DependsOn
+import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patcher.util.smali.ExternalLabel
+import app.revanced.patches.shared.annotation.YouTubeCompatibility
+import app.revanced.patches.youtube.layout.navigation.homepage.fingerprints.IntentExceptionFingerprint
+import app.revanced.patches.youtube.layout.navigation.homepage.fingerprints.LauncherActivityFingerprint
+import app.revanced.patches.youtube.misc.settings.resource.patch.SettingsPatch
+import app.revanced.util.integrations.Constants.NAVIGATION
+
+@Patch
+@Name("change-homepage")
+@Description("Change home page to subscription feed.")
+@DependsOn([SettingsPatch::class])
+@YouTubeCompatibility
+@Version("0.0.1")
+class ChangeHomePagePatch : BytecodePatch(
+    listOf(
+        IntentExceptionFingerprint,
+        LauncherActivityFingerprint
+    )
+) {
+    override fun execute(context: BytecodeContext): PatchResult {
+
+        LauncherActivityFingerprint.result?.mutableMethod?.let {
+            it.addInstructions(
+                it.implementation!!.instructions.size - 1, """
+                    move-object/from16 v0, p1
+                    invoke-static {v0}, $NAVIGATION->changeHomePage(Lcom/google/android/apps/youtube/app/watchwhile/WatchWhileActivity;)V
+                """
+            )
+        } ?: return LauncherActivityFingerprint.toErrorResult()
+
+        IntentExceptionFingerprint.result?.let {
+            with (it.mutableMethod) {
+                val index = it.scanResult.patternScanResult!!.endIndex + 1
+                addInstructions(
+                    index, """
+                    invoke-static {}, $NAVIGATION->changeHomePage()Z
+                    move-result v0
+                    if-eqz v0, :default
+                    return-void
+                    """, listOf(ExternalLabel("default", instruction(index)))
+                )
+            }
+        } ?: return IntentExceptionFingerprint.toErrorResult()
+
+        /*
+         * Add settings
+         */
+        SettingsPatch.addPreference(
+            arrayOf(
+                "PREFERENCE: NAVIGATION_SETTINGS",
+                "SETTINGS: CHANGE_HOMEPAGE_TO_SUBSCRIPTION"
+            )
+        )
+
+        SettingsPatch.updatePatchStatus("change-homepage")
+
+        return PatchResultSuccess()
+    }
+}
