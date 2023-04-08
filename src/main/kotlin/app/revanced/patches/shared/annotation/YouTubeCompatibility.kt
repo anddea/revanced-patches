1bc714c3da87b1489b55d13e39f82b8e211f638b
inotia00 <ars258000@gmail.com>
2023-04-09 00:37:21 +0900

    add support YouTube v18.13.38

--- src/main/kotlin/app/revanced/patches/shared/annotation/YouTubeCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/shared/annotation/YouTubeCompatibility.kt
@@ -12,7 +12,8 @@ import app.revanced.patcher.annotation.Package
             "18.09.39",
             "18.10.37",
             "18.11.36",
-            "18.12.35"
+            "18.12.35",
+            "18.13.38"
         )
     )]
 )
