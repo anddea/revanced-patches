0cab17189c9dfe262d97f4639be2a05cf286fe86
inotia00 <ars258000@gmail.com>
2023-04-16 13:37:57 +0900

    add `music-video-id-hook` patch

---
+++ src/main/kotlin/app/revanced/patches/music/misc/videoid/fingerprint/MusicVideoIdFingerprint.kt
@@ -0,0 +1,16 @@
+package app.revanced.patches.music.misc.videoid.fingerprint
+
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import org.jf.dexlib2.Opcode
+
+object MusicVideoIdFingerprint : MethodFingerprint(
+    returnType = "V",
+    parameters = listOf("L"),
+    opcodes = listOf(
+        Opcode.INVOKE_INTERFACE,
+        Opcode.MOVE_RESULT_OBJECT,
+        Opcode.INVOKE_INTERFACE,
+        Opcode.MOVE_RESULT_OBJECT
+    ),
+    customFingerprint = { it.definingClass.endsWith("SubtitlesOverlayPresenter;")  && it.name == "handleVideoStageEvent" }
+)
