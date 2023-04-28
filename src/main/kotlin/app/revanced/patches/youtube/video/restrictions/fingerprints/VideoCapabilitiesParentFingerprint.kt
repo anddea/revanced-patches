b4f98e42390ce4d627af2dc13ff2fc28e2cbd90b
inotia00 <ars258000@gmail.com>
2023-04-28 16:52:06 +0900

    remove `lift-vertical-video-restriction` patch (no longer valid)

--- src/main/kotlin/app/revanced/patches/youtube/video/restrictions/fingerprints/VideoCapabilitiesParentFingerprint.kt
+++
@@ -1,8 +0,0 @@
-package app.revanced.patches.youtube.video.restrictions.fingerprints
-
-import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
-
-object VideoCapabilitiesParentFingerprint : MethodFingerprint(
-    returnType = "L",
-    strings = listOf("minh.", ";maxh.")
-)
