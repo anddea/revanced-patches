3bc99a4a97474f33c75436472f95df9c8636adc6
inotia00 <ars258000@gmail.com>
2023-05-03 23:58:27 +0900

    add `hide-navigation-label` patch

---
+++ src/main/kotlin/app/revanced/patches/youtube/layout/navigation/label/fingerprints/PivotBarSetTextFingerprint.kt
@@ -0,0 +1,23 @@
+package app.revanced.patches.youtube.layout.navigation.label.fingerprints
+
+import app.revanced.patcher.extensions.or
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import org.jf.dexlib2.AccessFlags
+import org.jf.dexlib2.Opcode
+
+object PivotBarSetTextFingerprint : MethodFingerprint(
+    returnType = "V",
+    access = AccessFlags.PUBLIC or AccessFlags.CONSTRUCTOR,
+    parameters = listOf("L", "L", "L"),
+    opcodes = listOf(
+        Opcode.IPUT_OBJECT,
+        Opcode.INVOKE_DIRECT,
+        Opcode.INVOKE_VIRTUAL,
+        Opcode.IPUT_OBJECT,
+        Opcode.MOVE_OBJECT,
+        Opcode.CHECK_CAST,
+        Opcode.INVOKE_VIRTUAL,
+        Opcode.RETURN_VOID
+    ),
+    customFingerprint = { it.name == "<init>" }
+)
---
+++ src/main/kotlin/app/revanced/patches/youtube/layout/navigation/label/patch/NavigationLabelPatch.kt
@@ -0,0 +1,63 @@
+package app.revanced.patches.youtube.layout.navigation.label.patch
+
+import app.revanced.extensions.toErrorResult
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.addInstruction
+import app.revanced.patcher.extensions.instruction
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.annotations.DependsOn
+import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patches.shared.annotation.YouTubeCompatibility
+import app.revanced.patches.youtube.layout.navigation.label.fingerprints.PivotBarSetTextFingerprint
+import app.revanced.patches.youtube.misc.settings.resource.patch.SettingsPatch
+import app.revanced.util.integrations.Constants.NAVIGATION
+import org.jf.dexlib2.iface.instruction.OneRegisterInstruction
+import org.jf.dexlib2.iface.instruction.ReferenceInstruction
+
+@Patch
+@Name("hide-navigation-label")
+@Description("Hide navigation bar labels.")
+@DependsOn([SettingsPatch::class])
+@YouTubeCompatibility
+@Version("0.0.1")
+class NavigationLabelPatch : BytecodePatch(
+    listOf(
+        PivotBarSetTextFingerprint
+    )
+) {
+    override fun execute(context: BytecodeContext): PatchResult {
+
+        PivotBarSetTextFingerprint.result?.let {
+            with (it.mutableMethod) {
+                val targetIndex = it.scanResult.patternScanResult!!.endIndex - 2
+                val targetReference = (instruction(targetIndex) as ReferenceInstruction).reference.toString()
+                if (targetReference != "Landroid/widget/TextView;")
+                    return PivotBarSetTextFingerprint.toErrorResult()
+                val targetRegister = (instruction(targetIndex) as OneRegisterInstruction).registerA
+                addInstruction(
+                    targetIndex + 1,
+                    "invoke-static {v$targetRegister}, $NAVIGATION->hideNavigationLabel(Landroid/widget/TextView;)V"
+                )
+            }
+        } ?: return PivotBarSetTextFingerprint.toErrorResult()
+
+        /**
+         * Add settings
+         */
+        SettingsPatch.addPreference(
+            arrayOf(
+                "PREFERENCE: NAVIGATION_SETTINGS",
+                "SETTINGS: HIDE_NAVIGATION_LABEL"
+            )
+        )
+
+        SettingsPatch.updatePatchStatus("hide-navigation-label")
+
+        return PatchResultSuccess()
+    }
+}
