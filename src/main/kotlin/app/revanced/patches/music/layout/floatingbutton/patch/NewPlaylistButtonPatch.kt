a10eb967d1aa4970ff10c26032defa91906de8a3
inotia00 <ars258000@gmail.com>
2023-05-14 01:57:53 +0900

    feat(music/hide-new-playlist-button): change patch name https://github.com/inotia00/ReVanced_Extended/issues/983

--- src/main/kotlin/app/revanced/patches/music/layout/floatingbutton/patch/NewPlaylistButtonPatch.kt
+++ src/main/kotlin/app/revanced/patches/music/layout/floatingbutton/patch/NewPlaylistButtonPatch.kt
@@ -22,7 +22,7 @@ import app.revanced.util.enum.CategoryType
 import app.revanced.util.integrations.Constants.MUSIC_LAYOUT
 
 @Patch
-@Name("hide-new-playlist")
+@Name("hide-new-playlist-button")
 @Description("Hide the New Playlist button in the Library tab.")
 @DependsOn(
     [
