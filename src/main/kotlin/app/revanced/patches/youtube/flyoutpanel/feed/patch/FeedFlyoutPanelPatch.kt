196a1acefaa3394935360b668e9bda5baceaa7d7
inotia00 <ars258000@gmail.com>
2023-06-28 22:07:39 +0900

    feat(youtube): add `hide-feed-flyout-panel` patch

---
+++ src/main/kotlin/app/revanced/patches/youtube/flyoutpanel/feed/patch/FeedFlyoutPanelPatch.kt
@@ -0,0 +1,66 @@
+package app.revanced.patches.youtube.flyoutpanel.feed.patch
+
+import app.revanced.extensions.toErrorResult
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultError
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.annotations.DependsOn
+import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patches.youtube.flyoutpanel.feed.fingerprints.BottomSheetMenuItemBuilderFingerprint
+import app.revanced.patches.youtube.utils.annotations.YouTubeCompatibility
+import app.revanced.patches.youtube.utils.settings.resource.patch.SettingsPatch
+import app.revanced.util.integrations.Constants.FLYOUT_PANEL
+import org.jf.dexlib2.iface.instruction.OneRegisterInstruction
+import org.jf.dexlib2.iface.instruction.ReferenceInstruction
+
+@Patch
+@Name("hide-feed-flyout-panel")
+@Description("Hides feed flyout panel components.")
+@DependsOn([SettingsPatch::class])
+@YouTubeCompatibility
+@Version("0.0.1")
+class FeedFlyoutPanelPatch : BytecodePatch(
+    listOf(BottomSheetMenuItemBuilderFingerprint)
+) {
+    override fun execute(context: BytecodeContext): PatchResult {
+
+        BottomSheetMenuItemBuilderFingerprint.result?.let {
+            it.mutableMethod.apply {
+                val targetIndex = it.scanResult.patternScanResult!!.endIndex
+                val targetRegister = getInstruction<OneRegisterInstruction>(targetIndex).registerA
+
+                val targetParameter = getInstruction<ReferenceInstruction>(targetIndex - 1).reference
+                if (!targetParameter.toString().endsWith("Ljava/lang/CharSequence;"))
+                    return PatchResultError("Method signature parameter did not match: $targetParameter")
+
+                addInstructions(
+                    targetIndex + 1, """
+                        invoke-static {v$targetRegister}, $FLYOUT_PANEL->hideFeedFlyoutPanel(Ljava/lang/CharSequence;)Ljava/lang/CharSequence;
+                        move-result-object v$targetRegister
+                        """
+                )
+            }
+        } ?: return BottomSheetMenuItemBuilderFingerprint.toErrorResult()
+
+        /**
+         * Add settings
+         */
+        SettingsPatch.addPreference(
+            arrayOf(
+                "PREFERENCE: FLYOUT_PANEL_SETTINGS",
+                "SETTINGS: HIDE_FEED_FLYOUT_PANEL"
+            )
+        )
+
+        SettingsPatch.updatePatchStatus("hide-feed-flyout-panel")
+
+        return PatchResultSuccess()
+    }
+}
