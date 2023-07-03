9b4a3eb00af1a37edcab3a387ddf895ecf27bbd7
inotia00 <ars258000@gmail.com>
2023-07-03 19:52:30 +0900

    feat(youtube): drop support YouTube v18.19.36

--- src/main/kotlin/app/revanced/patches/youtube/utils/annotations/YouTubeCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/youtube/utils/annotations/YouTubeCompatibility.kt
@@ -6,7 +6,6 @@ import app.revanced.patcher.annotation.Package
 @Compatibility(
     [Package(
         "com.google.android.youtube", arrayOf(
-            "18.19.36",
             "18.20.39",
             "18.21.35",
             "18.22.37",
