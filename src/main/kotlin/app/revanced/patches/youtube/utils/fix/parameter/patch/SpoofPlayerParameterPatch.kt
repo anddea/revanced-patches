294f8eb36b4ca3a9d8fc4928d0be2d37d059ce5e
inotia00 <ars258000@gmail.com>
2023-07-11 21:56:09 +0900

    fix(youtube/spoof-player-parameter): show video time and chapters while using seekbar

--- src/main/kotlin/app/revanced/patches/youtube/utils/fix/parameter/patch/SpoofPlayerParameterPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/utils/fix/parameter/patch/SpoofPlayerParameterPatch.kt
@@ -8,6 +8,9 @@ import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.data.toMethodWalker
 import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.removeInstruction
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint.Companion.resolve
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.PatchResult
 import app.revanced.patcher.patch.PatchResultSuccess
@@ -16,16 +19,23 @@ import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod
 import app.revanced.patches.youtube.utils.annotations.YouTubeCompatibility
 import app.revanced.patches.youtube.utils.fix.parameter.fingerprints.BadResponseFingerprint
 import app.revanced.patches.youtube.utils.fix.parameter.fingerprints.ProtobufParameterBuilderFingerprint
+import app.revanced.patches.youtube.utils.fix.parameter.fingerprints.ScrubbedPreviewLayoutFingerprint
+import app.revanced.patches.youtube.utils.fix.parameter.fingerprints.StoryboardThumbnailFingerprint
+import app.revanced.patches.youtube.utils.fix.parameter.fingerprints.StoryboardThumbnailParentFingerprint
 import app.revanced.patches.youtube.utils.fix.parameter.fingerprints.SubtitleWindowFingerprint
 import app.revanced.patches.youtube.utils.playertype.patch.PlayerTypeHookPatch
+import app.revanced.patches.youtube.utils.resourceid.patch.SharedResourceIdPatch
 import app.revanced.patches.youtube.utils.settings.resource.patch.SettingsPatch
 import app.revanced.patches.youtube.utils.videoid.general.patch.VideoIdPatch
 import app.revanced.util.integrations.Constants.MISC_PATH
+import org.jf.dexlib2.iface.instruction.OneRegisterInstruction
+import org.jf.dexlib2.iface.instruction.ReferenceInstruction
 
 @Name("spoof-player-parameters")
 @Description("Spoofs player parameters to prevent playback issues.")
 @DependsOn(
     [
+        SharedResourceIdPatch::class,
         PlayerTypeHookPatch::class,
         VideoIdPatch::class
     ]
@@ -36,6 +46,8 @@ class SpoofPlayerParameterPatch : BytecodePatch(
     listOf(
         BadResponseFingerprint,
         ProtobufParameterBuilderFingerprint,
+        ScrubbedPreviewLayoutFingerprint,
+        StoryboardThumbnailParentFingerprint,
         SubtitleWindowFingerprint
     )
 ) {
@@ -59,6 +71,51 @@ class SpoofPlayerParameterPatch : BytecodePatch(
                     )
                 }
         } ?: return ProtobufParameterBuilderFingerprint.toErrorResult()
+
+        // When the player parameter is spoofed in incognito mode, this value will always be false
+        // If this value is true, the timestamp and chapter are showned when tapping the seekbar.
+        StoryboardThumbnailParentFingerprint.result?.classDef?.let { classDef ->
+            StoryboardThumbnailFingerprint.also {
+                it.resolve(
+                    context,
+                    classDef
+                )
+            }.result?.let {
+                it.mutableMethod.apply {
+                    val targetIndex = it.scanResult.patternScanResult!!.endIndex
+                    val targetRegister = getInstruction<OneRegisterInstruction>(targetIndex).registerA
+
+                    // Since this is end of the method must replace one line then add the rest.
+                    addInstructions(
+                        targetIndex + 1,
+                        """
+                            invoke-static {v$targetRegister}, $INTEGRATIONS_CLASS_DESCRIPTOR->getSeekbarThumbnailOverrideValue(Z)Z
+                            move-result v$targetRegister
+                            return v$targetRegister
+                            """
+                    )
+                    removeInstruction(targetIndex)
+                }
+            } ?: return StoryboardThumbnailFingerprint.toErrorResult()
+        } ?: return StoryboardThumbnailParentFingerprint.toErrorResult()
+
+        // Seekbar thumbnail now show up but are always a blank image.
+        // Additional changes are needed to force the client to generate the thumbnails (assuming it's possible),
+        // but for now hide the empty thumbnail.
+        ScrubbedPreviewLayoutFingerprint.result?.let {
+            it.mutableMethod.apply {
+                val endIndex = it.scanResult.patternScanResult!!.endIndex
+                val imageViewFieldName = getInstruction<ReferenceInstruction>(endIndex).reference
+
+                addInstructions(
+                    implementation!!.instructions.lastIndex,
+                    """
+                        iget-object v0, p0, $imageViewFieldName   # copy imageview field to a register
+                        invoke-static {v0}, $INTEGRATIONS_CLASS_DESCRIPTOR->seekbarImageViewCreated(Landroid/widget/ImageView;)V
+                        """
+                )
+            }
+        } ?: return ScrubbedPreviewLayoutFingerprint.toErrorResult()
 
         // hook video playback result
         BadResponseFingerprint.result?.mutableMethod?.addInstruction(
