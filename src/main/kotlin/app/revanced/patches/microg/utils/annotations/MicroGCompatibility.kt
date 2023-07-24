03b9875d164a465165dbbe2fde5789c8adcd9353
inotia00 <ars258000@gmail.com>
2023-07-25 00:53:38 +0900

    feat(microg): temporary suspension of support for all patches https://github.com/inotia00/ReVanced_Extended/issues/1207

--- src/main/kotlin/app/revanced/patches/microg/utils/annotations/MicroGCompatibility.kt
+++
@@ -1,8 +0,0 @@
-package app.revanced.patches.microg.utils.annotations
-
-import app.revanced.patcher.annotation.Compatibility
-import app.revanced.patcher.annotation.Package
-
-@Compatibility([Package("com.mgoogle.android.gms")])
-@Target(AnnotationTarget.CLASS)
-internal annotation class MicroGCompatibility
