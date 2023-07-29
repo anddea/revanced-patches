fa76fe94578cc751631a135590bcaf599522056c
inotia00 <ars258000@gmail.com>
2023-07-30 08:49:59 +0900

    fix(youtube/hide-shorts-components): wrong case notation

--- src/main/kotlin/app/revanced/patches/youtube/shorts/shortscomponent/patch/ShortsComponentPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/shorts/shortscomponent/patch/ShortsComponentPatch.kt
@@ -18,7 +18,7 @@ import app.revanced.patches.youtube.utils.settings.resource.patch.SettingsPatch
 import app.revanced.util.integrations.Constants.PATCHES_PATH
 
 @Patch
-@Name("Hide Shorts components")
+@Name("Hide shorts components")
 @Description("Hides other Shorts components.")
 @DependsOn(
     [
