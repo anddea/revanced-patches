3e8d3da0f3f03440ab584d0fa32019e9cc1a911b
inotia00 <ars258000@gmail.com>
2023-09-05 10:46:39 +0900

    feat(music) delete `share-button-hook` patch

--- src/main/kotlin/app/revanced/patches/music/misc/sharebuttonhook/fingerprints/FullStackTraceActivityFingerprint.kt
+++
@@ -1,7 +0,0 @@
-package app.revanced.patches.music.misc.sharebuttonhook.fingerprints
-
-import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
-
-object FullStackTraceActivityFingerprint : MethodFingerprint(
-    customFingerprint = { methodDef, _ -> methodDef.definingClass.endsWith("/FullStackTraceActivity;") && methodDef.name == "onCreate" }
-)
