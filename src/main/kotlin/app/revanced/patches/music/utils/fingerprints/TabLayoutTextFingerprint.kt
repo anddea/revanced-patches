0a27088c8d9bc34da792eeef0233f82f3f32d5ae
inotia00 <ars258000@gmail.com>
2023-09-05 12:10:09 +0900

    fix: compile error

---
+++ src/main/kotlin/app/revanced/patches/music/utils/fingerprints/TabLayoutTextFingerprint.kt
@@ -0,0 +1,25 @@
+package app.revanced.patches.music.utils.fingerprints
+
+import app.revanced.patcher.extensions.or
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import app.revanced.patches.music.utils.resourceid.patch.SharedResourceIdPatch.Companion.Text1
+import app.revanced.util.bytecode.isWideLiteralExists
+import com.android.tools.smali.dexlib2.AccessFlags
+import com.android.tools.smali.dexlib2.Opcode
+
+object TabLayoutTextFingerprint : MethodFingerprint(
+    returnType = "V",
+    accessFlags = AccessFlags.PUBLIC or AccessFlags.FINAL,
+    parameters = listOf("L"),
+    opcodes = listOf(
+        Opcode.IGET,
+        Opcode.INVOKE_STATIC,
+        Opcode.MOVE_RESULT_OBJECT,
+        Opcode.IF_NEZ,
+        Opcode.SGET_OBJECT,
+        Opcode.INVOKE_INTERFACE,
+        Opcode.MOVE_RESULT
+    ),
+    customFingerprint = { methodDef, _ -> methodDef.isWideLiteralExists(Text1) }
+)
+
