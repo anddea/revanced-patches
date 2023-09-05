c5a68bf7142e2e4f7fb6d4dc83caecfceacd9e59
inotia00 <ars258000@gmail.com>
2023-09-05 12:21:52 +0900

    feat(music): add compatibility version constraints (ryd does not support older versions)

--- src/main/kotlin/app/revanced/patches/music/utils/annotations/MusicCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/music/utils/annotations/MusicCompatibility.kt
@@ -3,6 +3,16 @@ package app.revanced.patches.music.utils.annotations
 import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
-@Compatibility([Package("com.google.android.apps.youtube.music")])
+@Compatibility(
+    [
+        Package(
+            "com.google.android.apps.youtube.music",
+            arrayOf(
+                "6.15.52",
+                "6.17.52"
+            )
+        )
+    ]
+)
 @Target(AnnotationTarget.CLASS)
 internal annotation class MusicCompatibility
