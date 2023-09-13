a93365acb3b38cc56278ec1becd9472a7619c031
inotia00 <ars258000@gmail.com>
2023-09-13 12:27:50 +0900

    fix(music/spoof-app-version): add missing dependencies

--- src/main/kotlin/app/revanced/patches/music/misc/versionspoof/patch/SpoofAppVersionPatch.kt
+++ src/main/kotlin/app/revanced/patches/music/misc/versionspoof/patch/SpoofAppVersionPatch.kt
@@ -5,6 +5,8 @@ import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.patch.annotations.DependsOn
 import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patches.music.general.oldstylelibraryshelf.patch.OldStyleLibraryShelfPatch
+import app.revanced.patches.music.navigation.sample.patch.SampleButtonPatch
 import app.revanced.patches.music.utils.annotations.MusicCompatibility
 import app.revanced.patches.music.utils.settings.resource.patch.SettingsPatch
 import app.revanced.patches.shared.patch.versionspoof.AbstractVersionSpoofPatch
@@ -14,7 +16,13 @@ import app.revanced.util.integrations.Constants.MUSIC_MISC_PATH
 @Patch
 @Name("Spoof app version")
 @Description("Spoof the YouTube Music client version.")
-@DependsOn([SettingsPatch::class])
+@DependsOn(
+    [
+        OldStyleLibraryShelfPatch::class,
+        SampleButtonPatch::class,
+        SettingsPatch::class
+    ]
+)
 @MusicCompatibility
 class SpoofAppVersionPatch : AbstractVersionSpoofPatch(
     "$MUSIC_MISC_PATH/SpoofAppVersionPatch;->getVersionOverride(Ljava/lang/String;)Ljava/lang/String;"
