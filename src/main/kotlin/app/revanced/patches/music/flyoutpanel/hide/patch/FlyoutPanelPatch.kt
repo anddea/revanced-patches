650d0d9fffe12f52964a5a644d0ea48212c698de
inotia00 <108592928+inotia00@users.noreply.github.com>
2023-10-02 22:30:32 +0900

    fix(music/enable-playback-speed): excluding enable playback speed patch still shows playback speed icon in the flyout menu

--- src/main/kotlin/app/revanced/patches/music/flyoutpanel/hide/patch/FlyoutPanelPatch.kt
+++ src/main/kotlin/app/revanced/patches/music/flyoutpanel/hide/patch/FlyoutPanelPatch.kt
@@ -13,7 +13,7 @@ import app.revanced.patcher.util.smali.ExternalLabel
 import app.revanced.patches.music.flyoutpanel.utils.EnumUtils.getEnumIndex
 import app.revanced.patches.music.utils.annotations.MusicCompatibility
 import app.revanced.patches.music.utils.fingerprints.MenuItemFingerprint
-import app.revanced.patches.music.utils.flyoutbutton.patch.FlyoutButtonContainerResourcePatch
+import app.revanced.patches.music.utils.flyoutbutton.patch.FlyoutButtonContainerPatch
 import app.revanced.patches.music.utils.settings.resource.patch.SettingsPatch
 import app.revanced.util.enum.CategoryType
 import app.revanced.util.integrations.Constants.MUSIC_FLYOUT
@@ -27,7 +27,7 @@ import com.android.tools.smali.dexlib2.iface.instruction.TwoRegisterInstruction
 @Description("Hides flyout panel components.")
 @DependsOn(
     [
-        FlyoutButtonContainerResourcePatch::class,
+        FlyoutButtonContainerPatch::class,
         SettingsPatch::class
     ]
 )
--- src/main/kotlin/app/revanced/patches/music/utils/flyoutbutton/fingerprints/FlyoutPanelLikeButtonFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/music/utils/flyoutbutton/fingerprints/FlyoutPanelLikeButtonFingerprint.kt
@@ -1,4 +1,4 @@
-package app.revanced.patches.music.flyoutpanel.playbackspeed.fingerprints
+package app.revanced.patches.music.utils.flyoutbutton.fingerprints
 
 import app.revanced.patcher.extensions.or
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
