8779a990650bb228001ff39e065848e537f7f65c
inotia00 <108592928+inotia00@users.noreply.github.com>
2023-10-12 17:28:24 +0900

    feat(YouTube Music/Enable new player layout): change patch name `Enable new layout` → `Enable new player layout`

--- src/main/kotlin/app/revanced/patches/music/utils/playerlayouthook/fingerprints/NewPlayerLayoutFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/music/utils/playerlayouthook/fingerprints/NewPlayerLayoutFingerprint.kt
@@ -1,9 +1,9 @@
-package app.revanced.patches.music.player.newlayout.fingerprints
+package app.revanced.patches.music.utils.playerlayouthook.fingerprints
 
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
 import app.revanced.util.bytecode.isWide32LiteralExists
 
-object NewLayoutFingerprint : MethodFingerprint(
+object NewPlayerLayoutFingerprint : MethodFingerprint(
     returnType = "Z",
     parameters = emptyList(),
     customFingerprint = { methodDef, _ -> methodDef.isWide32LiteralExists(45399578) }
