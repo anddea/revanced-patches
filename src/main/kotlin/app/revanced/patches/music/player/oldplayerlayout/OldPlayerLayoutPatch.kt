515ad09e2ccedb3b105e4809f531691dcc4f9990
inotia00 <108592928+inotia00@users.noreply.github.com>
2023-12-08 11:32:17 +0900

    fix(YouTube Music/Enable old player layout): add version constraint (no longer available as legacy code has been completely removed in `v6.31.55+`)

--- src/main/kotlin/app/revanced/patches/music/player/oldplayerlayout/OldPlayerLayoutPatch.kt
+++ src/main/kotlin/app/revanced/patches/music/player/oldplayerlayout/OldPlayerLayoutPatch.kt
@@ -5,7 +5,7 @@ import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
-import app.revanced.patches.music.utils.fingerprints.NewPlayerLayoutFingerprint
+import app.revanced.patches.music.player.oldplayerlayout.fingerprints.OldPlayerLayoutFingerprint
 import app.revanced.patches.music.utils.integrations.Constants.PLAYER
 import app.revanced.patches.music.utils.settings.CategoryType
 import app.revanced.patches.music.utils.settings.SettingsPatch
@@ -15,16 +15,24 @@ import app.revanced.util.exception
     name = "Enable old player layout",
     description = "Return the player layout to old style.",
     dependencies = [SettingsPatch::class],
-    compatiblePackages = [CompatiblePackage("com.google.android.apps.youtube.music")],
+    compatiblePackages = [
+        CompatiblePackage(
+            "com.google.android.apps.youtube.music",
+            [
+                "6.21.52",
+                "6.29.58"
+            ]
+        )
+    ],
     use = false
 )
 @Suppress("unused")
 object OldPlayerLayoutPatch : BytecodePatch(
-    setOf(NewPlayerLayoutFingerprint)
+    setOf(OldPlayerLayoutFingerprint)
 ) {
     override fun execute(context: BytecodeContext) {
 
-        NewPlayerLayoutFingerprint.result?.let {
+        OldPlayerLayoutFingerprint.result?.let {
             it.mutableMethod.apply {
                 addInstructions(
                     0, """
@@ -34,7 +42,7 @@ object OldPlayerLayoutPatch : BytecodePatch(
                         """
                 )
             }
-        } ?: throw NewPlayerLayoutFingerprint.exception
+        } ?: throw OldPlayerLayoutFingerprint.exception
 
         SettingsPatch.addMusicPreference(
             CategoryType.PLAYER,
--- src/main/kotlin/app/revanced/patches/music/utils/fingerprints/NewPlayerLayoutFingerprint.kt
+++
@@ -1,9 +0,0 @@
-package app.revanced.patches.music.utils.fingerprints
-
-import app.revanced.util.fingerprint.LiteralValueFingerprint
-
-object NewPlayerLayoutFingerprint : LiteralValueFingerprint(
-    returnType = "Z",
-    parameters = emptyList(),
-    literalSupplier = { 45399578 }
-)
