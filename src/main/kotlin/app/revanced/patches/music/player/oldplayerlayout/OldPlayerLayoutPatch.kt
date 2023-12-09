ad4fb4025715a5d2ba6311ff52c25bbb25c45100
inotia00 <108592928+inotia00@users.noreply.github.com>
2023-12-09 16:40:25 +0900

    fix(YouTube Music/Enable old player layout): add version constraint

--- src/main/kotlin/app/revanced/patches/music/player/oldplayerlayout/OldPlayerLayoutPatch.kt
+++ src/main/kotlin/app/revanced/patches/music/player/oldplayerlayout/OldPlayerLayoutPatch.kt
@@ -3,27 +3,19 @@ package app.revanced.patches.music.player.oldplayerlayout
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.PatchException
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.music.player.oldplayerlayout.fingerprints.OldPlayerLayoutFingerprint
 import app.revanced.patches.music.utils.integrations.Constants.PLAYER
 import app.revanced.patches.music.utils.settings.CategoryType
 import app.revanced.patches.music.utils.settings.SettingsPatch
-import app.revanced.util.exception
 
 @Patch(
     name = "Enable old player layout",
-    description = "Return the player layout to old style.",
+    description = "Return the player layout to old style. Deprecated on YT Music 6.31.55+",
     dependencies = [SettingsPatch::class],
-    compatiblePackages = [
-        CompatiblePackage(
-            "com.google.android.apps.youtube.music",
-            [
-                "6.21.52",
-                "6.29.58"
-            ]
-        )
-    ],
+    compatiblePackages = [CompatiblePackage("com.google.android.apps.youtube.music")],
     use = false
 )
 @Suppress("unused")
@@ -42,7 +34,7 @@ object OldPlayerLayoutPatch : BytecodePatch(
                         """
                 )
             }
-        } ?: throw OldPlayerLayoutFingerprint.exception
+        } ?: throw PatchException("This version is not supported. Please use YT Music 6.29.58 or earlier.")
 
         SettingsPatch.addMusicPreference(
             CategoryType.PLAYER,
