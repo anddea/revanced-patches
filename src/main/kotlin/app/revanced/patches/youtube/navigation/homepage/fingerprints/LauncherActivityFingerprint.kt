6a4ca210df9a3e70b99c5544a3313f6559f24525
inotia00 <108592928+inotia00@users.noreply.github.com>
2023-12-12 18:26:12 +0900

    feat(YouTube): remove `Change homepage` patch

--- src/main/kotlin/app/revanced/patches/youtube/navigation/homepage/fingerprints/LauncherActivityFingerprint.kt
+++
@@ -1,8 +0,0 @@
-package app.revanced.patches.youtube.navigation.homepage.fingerprints
-
-import app.revanced.patcher.fingerprint.MethodFingerprint
-
-object LauncherActivityFingerprint : MethodFingerprint(
-    returnType = "V",
-    strings = listOf("forLauncherActivity", "Launcher config used on invalid activity: %s")
-)
