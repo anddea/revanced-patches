dcd5ffe82e07f672b5ef320588c3630913d40f3e
inotia00 <108592928+inotia00@users.noreply.github.com>
2023-12-28 21:44:25 +0900

    fix(YouTube Music/Disable auto captions): captions are always disabled when `Disable auto captions` is on

--- src/main/kotlin/app/revanced/patches/music/video/videoid/VideoIdPatch.kt
+++ src/main/kotlin/app/revanced/patches/music/video/videoid/VideoIdPatch.kt
@@ -2,29 +2,34 @@ package app.revanced.patches.music.video.videoid
 
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.extensions.InstructionExtensions.addInstructionsWithLabels
 import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
+import app.revanced.patcher.extensions.InstructionExtensions.replaceInstruction
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod
+import app.revanced.patcher.util.smali.ExternalLabel
 import app.revanced.patches.music.video.videoid.fingerprints.PlayerResponseModelStoryboardRendererFingerprint
 import app.revanced.patches.music.video.videoid.fingerprints.VideoIdParentFingerprint
 import app.revanced.util.exception
 import com.android.tools.smali.dexlib2.Opcode
 import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 import com.android.tools.smali.dexlib2.iface.instruction.ReferenceInstruction
+import com.android.tools.smali.dexlib2.iface.instruction.TwoRegisterInstruction
 import com.android.tools.smali.dexlib2.iface.reference.FieldReference
 import com.android.tools.smali.dexlib2.iface.reference.MethodReference
+import java.io.Closeable
 
 object VideoIdPatch : BytecodePatch(
     setOf(
         PlayerResponseModelStoryboardRendererFingerprint,
         VideoIdParentFingerprint
     )
-) {
+), Closeable {
     private var videoIdRegister = 0
     private var videoIdInsertIndex = 0
     private lateinit var videoIdMethod: MutableMethod
 
-    private var backgroundPlaybackVideoIdRegister = 0
     private var backgroundPlaybackInsertIndex = 0
     private var backgroundPlaybackMethodName = ""
     private lateinit var backgroundPlaybackMethod: MutableMethod
@@ -57,8 +62,42 @@ object VideoIdPatch : BytecodePatch(
             ?.apply {
                 backgroundPlaybackMethod = this
                 backgroundPlaybackInsertIndex = implementation!!.instructions.size - 1
-                backgroundPlaybackVideoIdRegister = getInstruction<OneRegisterInstruction>(backgroundPlaybackInsertIndex).registerA
             } ?: throw PlayerResponseModelStoryboardRendererFingerprint.exception
+    }
+
+    override fun close () {
+        backgroundPlaybackMethod.apply {
+            val videoIdIndex = implementation!!.instructions.indexOfLast {
+                it.opcode == Opcode.IGET_OBJECT
+            }
+            val videoIdRegister = getInstruction<TwoRegisterInstruction>(videoIdIndex).registerB
+            val videoIdReference = getInstruction<ReferenceInstruction>(videoIdIndex).reference
+            val videoIdInstructionCall = "iget-object p0, v$videoIdRegister, $videoIdReference"
+
+            if (backgroundPlaybackInsertIndex != videoIdIndex + 1) {
+                replaceInstruction(
+                    backgroundPlaybackInsertIndex,
+                    "return-object p0"
+                )
+                replaceInstruction(
+                    videoIdIndex,
+                    videoIdInstructionCall
+                )
+                addInstruction(
+                    backgroundPlaybackInsertIndex,
+                    videoIdInstructionCall
+                )
+                addInstructionsWithLabels(
+                    videoIdIndex + 1, """
+                        if-eqz p0, :ignore
+                        invoke-virtual {p0}, Ljava/lang/String;->isEmpty()Z
+                        move-result p0
+                        if-nez p0, :ignore
+                        $videoIdInstructionCall
+                        """, ExternalLabel("ignore", getInstruction(backgroundPlaybackInsertIndex))
+                )
+            }
+        }
     }
 
     fun hookVideoId(
@@ -72,7 +111,7 @@ object VideoIdPatch : BytecodePatch(
         methodDescriptor: String
     ) = backgroundPlaybackMethod.addInstruction(
         backgroundPlaybackInsertIndex++, // move-result-object offset
-        "invoke-static {v$backgroundPlaybackVideoIdRegister}, $methodDescriptor"
+        "invoke-static {p0}, $methodDescriptor"
     )
 }
 
