152319da76a703dd96782417ce70224af578d603
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-01-23 20:13:50 +0900

    feat(Reddit): add `Remove subreddit dialog` patch

--- src/main/kotlin/app/revanced/patches/reddit/utils/resourceid/SharedResourceIdPatch.kt
+++ src/main/kotlin/app/revanced/patches/reddit/utils/resourceid/SharedResourceIdPatch.kt
@@ -7,11 +7,14 @@ import app.revanced.patches.shared.patch.mapping.ResourceMappingPatch
 import app.revanced.patches.shared.patch.mapping.ResourceType
 import app.revanced.patches.shared.patch.mapping.ResourceType.ID
 import app.revanced.patches.shared.patch.mapping.ResourceType.STRING
+import app.revanced.patches.shared.patch.mapping.ResourceType.STYLE
 
 @Patch(dependencies = [ResourceMappingPatch::class])
 object SharedResourceIdPatch : ResourcePatch() {
+    var CancelButton: Long = -1L
     var LabelAcknowledgements: Long = -1L
     var ScreenShotShareBanner: Long = -1L
+    var TextAppearanceRedditBaseOldButtonColored: Long = -1L
     var ToolBarNavSearchCtaContainer: Long = -1L
 
     override fun execute(context: ResourceContext) {
@@ -21,8 +24,10 @@ object SharedResourceIdPatch : ResourcePatch() {
             .find { it.type == resourceType.value && it.name == resourceName }?.id
             ?: -1
 
+        CancelButton = find(ID, "cancel_button")
         LabelAcknowledgements = find(STRING, "label_acknowledgements")
         ScreenShotShareBanner = find(STRING, "screenshot_share_banner_title")
+        TextAppearanceRedditBaseOldButtonColored = find(STYLE, "TextAppearance.RedditBase.OldButton.Colored")
         ToolBarNavSearchCtaContainer = find(ID, "toolbar_nav_search_cta_container")
 
     }
