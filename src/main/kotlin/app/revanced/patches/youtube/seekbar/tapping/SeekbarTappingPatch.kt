e783f18df0f3d53ba3f91f4a25ce52270939d58f
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-01-25 07:06:46 +0900

    feat(YouTube): add support versions `18.46.45`, `18.48.49`, `18.49.37`, `19.01.34`, `19.02.39`

--- src/main/kotlin/app/revanced/patches/youtube/seekbar/tapping/SeekbarTappingPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/seekbar/tapping/SeekbarTappingPatch.kt
@@ -44,7 +44,12 @@ import com.android.tools.smali.dexlib2.iface.instruction.formats.Instruction11n
                 "18.42.41",
                 "18.43.45",
                 "18.44.41",
-                "18.45.43"
+                "18.45.43",
+                "18.46.45",
+                "18.48.39",
+                "18.49.37",
+                "19.01.34",
+                "19.02.39"
             ]
         )
     ]
