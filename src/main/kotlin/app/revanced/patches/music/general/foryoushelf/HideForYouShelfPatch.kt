2d5f41c360755f073afdeda4626a8e11ef5867c2
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2024-01-31 16:34:14 +0700

    feat(YTM): Add `Hide For You shelf`

---
+++ src/main/kotlin/app/revanced/patches/music/general/foryoushelf/HideForYouShelfPatch.kt
@@ -0,0 +1,53 @@
+package app.revanced.patches.music.general.foryoushelf
+
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.patches.music.utils.integrations.Constants.COMPONENTS_PATH
+import app.revanced.patches.music.utils.litho.LithoFilterPatch
+import app.revanced.patches.music.utils.settings.CategoryType
+import app.revanced.patches.music.utils.settings.SettingsPatch
+
+@Patch(
+    name = "Hide For You shelf",
+    description = "Adds an option to hide the For You shelf from the homepage.",
+    dependencies = [
+        LithoFilterPatch::class,
+        SettingsPatch::class
+    ],
+    compatiblePackages = [
+        CompatiblePackage(
+            "com.google.android.apps.youtube.music",
+            [
+                "6.21.52",
+                "6.22.52",
+                "6.23.56",
+                "6.25.53",
+                "6.26.51",
+                "6.27.54",
+                "6.28.53",
+                "6.29.58",
+                "6.31.55",
+                "6.33.52"
+            ]
+        )
+    ]
+)
+@Suppress("unused")
+object HideCarouselShelfPatch : BytecodePatch(emptySet()) {
+    override fun execute(context: BytecodeContext) {
+
+        SettingsPatch.addMusicPreference(
+            CategoryType.GENERAL,
+            "revanced_hide_for_you_shelf",
+            "false"
+        )
+
+        LithoFilterPatch.addFilter(FILTER_CLASS_DESCRIPTOR)
+
+    }
+
+    private const val FILTER_CLASS_DESCRIPTOR =
+        "$COMPONENTS_PATH/ForYouShelfFilter;"
+}
