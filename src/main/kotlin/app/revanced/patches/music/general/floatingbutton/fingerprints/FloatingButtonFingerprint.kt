40ea2ecd756b566f43ee3838c2744c0fce8f6bab
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2024-02-13 17:01:24 +0700

    feat(Hide new playlist button): Apply fingerprint compatible with the wider version

--- src/main/kotlin/app/revanced/patches/music/general/floatingbutton/fingerprints/FloatingButtonFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/music/general/floatingbutton/fingerprints/FloatingButtonFingerprint.kt
@@ -2,13 +2,15 @@ package app.revanced.patches.music.general.floatingbutton.fingerprints
 
 import app.revanced.patcher.extensions.or
 import app.revanced.patcher.fingerprint.MethodFingerprint
-import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.Opcode
 
 object FloatingButtonFingerprint : MethodFingerprint(
     returnType = "V",
-    accessFlags = AccessFlags.PRIVATE or AccessFlags.FINAL,
     parameters = listOf("L"),
-    opcodes = listOf(Opcode.AND_INT_LIT16)
+    opcodes = listOf(
+        Opcode.IGET,
+        Opcode.AND_INT_LIT16,
+        Opcode.IF_EQZ
+    )
 )
 
