289cc6e9b5e8371daa48709ef4838c7dd1f01f28
Aaron Veil <70171475+anddea@users.noreply.github.com>
2024-03-05 19:47:35 +0300

    feat(YouTube/Settings icons) Add more icons

--- src/main/kotlin/app/revanced/patches/youtube/utils/settings/SettingsAddIconsPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/utils/settings/SettingsAddIconsPatch.kt
@@ -12,7 +12,7 @@ import org.w3c.dom.Element
     name = "Settings icons",
     description = "Adds icons to specific preferences in the settings.",
     dependencies = [SettingsPatch::class],
-    compatiblePackages = [CompatiblePackage("com.google.android.youtube", [])], // Adjust package name and version code
+    compatiblePackages = [CompatiblePackage("com.google.android.youtube", [])],
     use = true
 )
 @Suppress("unused")
@@ -20,13 +20,67 @@ object SettingsAddIconsPatch : ResourcePatch() {
     override fun execute(context: ResourceContext) {
 
         val validTitles = setOf(
+            "revanced_change_player_flyout_panel_toggle",
+            "revanced_disable_hdr_auto_brightness",
+            "revanced_enable_bottom_player_gestures",
+            "revanced_enable_old_quality_layout",
+            "revanced_enable_swipe_auto_brightness",
+            "revanced_enable_swipe_brightness",
+            "revanced_enable_swipe_volume",
+            "revanced_hide_button_download",
+            "revanced_hide_button_like_dislike",
+            "revanced_hide_button_remix",
+            "revanced_hide_button_report",
+            "revanced_hide_button_save_to_playlist",
+            "revanced_hide_button_share",
+            "revanced_hide_create_button",
+            "revanced_hide_home_button",
+            "revanced_hide_library_button",
+            "revanced_hide_notifications_button",
+            "revanced_hide_player_flyout_panel_ambient_mode",
+            "revanced_hide_player_flyout_panel_audio_track",
+            "revanced_hide_player_flyout_panel_captions",
+            "revanced_hide_player_flyout_panel_help",
+            "revanced_hide_player_flyout_panel_loop_video",
+            "revanced_hide_player_flyout_panel_more_info",
+            "revanced_hide_player_flyout_panel_playback_speed",
+            "revanced_hide_player_flyout_panel_report",
+            "revanced_hide_player_flyout_panel_stable_volume",
+            "revanced_hide_player_flyout_panel_stats_for_nerds",
+            "revanced_hide_player_flyout_panel_watch_in_vr",
+            "revanced_hide_quick_actions_comment",
+            "revanced_hide_quick_actions_dislike",
+            "revanced_hide_quick_actions_like",
+            "revanced_hide_quick_actions_more",
+            "revanced_hide_quick_actions_save_to_playlist",
+            "revanced_hide_quick_actions_share",
+            "revanced_hide_shorts_button",
+            "revanced_hide_shorts_player_comments_button",
+            "revanced_hide_shorts_player_dislike_button",
+            "revanced_hide_shorts_player_like_button",
+            "revanced_hide_shorts_player_remix_button",
+            "revanced_hide_shorts_player_share_button",
+            "revanced_hide_shorts_toolbar_camera_button",
+            "revanced_hide_shorts_toolbar_menu_button",
+            "revanced_hide_shorts_toolbar_search_button",
+            "revanced_hide_subscriptions_button",
             "revanced_overlay_button_always_repeat",
-            "revanced_overlay_button_copy_video_url_timestamp",
             "revanced_overlay_button_copy_video_url",
+            "revanced_overlay_button_copy_video_url_timestamp",
+            "revanced_overlay_button_external_downloader",
             "revanced_overlay_button_speed_dialog",
             "revanced_overlay_button_time_ordered_playlist",
             "revanced_overlay_button_whitelisting",
-            "revanced_overlay_button_external_downloader"
+            "revanced_switching_create_notification",
+            "sb_enable_create_segment",
+            "sb_enable_voting",
+        )
+
+        val emptyTitles = setOf(
+            "external_downloader",
+            "revanced_hide_player_flyout_panel_captions_footer",
+            "revanced_hide_player_flyout_panel_quality_footer",
+            "whitelisting",
         )
 
         arrayOf(
@@ -47,6 +101,8 @@ object SettingsAddIconsPatch : ResourcePatch() {
                 if (title in validTitles) {
                     val drawableName = title + "_icon"
                     preference?.setAttribute("android:icon", "@drawable/$drawableName")
+                } else if (title in emptyTitles) {
+                    preference?.setAttribute("android:icon", "@drawable/empty_icon")
                 }
             }
 
@@ -54,8 +110,8 @@ object SettingsAddIconsPatch : ResourcePatch() {
             for (i in 0 until preferenceScreens.length) {
                 val preference = preferenceScreens.item(i) as? Element
                 val title = preference?.getAttribute("android:key")
-                if (title == "whitelisting" || title == "external_downloader") {
-                    preference.setAttribute("android:icon", "@drawable/empty_icon")
+                if (title in emptyTitles) {
+                    preference?.setAttribute("android:icon", "@drawable/empty_icon")
                 }
             }
         }
