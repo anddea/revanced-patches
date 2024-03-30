4575e86de5405417dead2cc2ab7cc509144fafaf
Aaron Veil <70171475+anddea@users.noreply.github.com>
2024-03-30 13:13:21 +0300

    fix(YT Music): Support latest versions

--- src/main/kotlin/app/revanced/patches/music/misc/backgroundplay/BackgroundPlayPatch.kt
+++ src/main/kotlin/app/revanced/patches/music/misc/backgroundplay/BackgroundPlayPatch.kt
@@ -1,11 +1,12 @@
 package app.revanced.patches.music.misc.backgroundplay
 
 import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
 import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
-import app.revanced.patcher.util.proxy.mutableTypes.MutableMethod
+import app.revanced.patches.music.layout.minimizedplayback.fingerprints.KidsMinimizedPlaybackPolicyControllerFingerprint
 import app.revanced.patches.music.misc.backgroundplay.fingerprints.BackgroundPlaybackParentFingerprint
 import app.revanced.util.exception
 
@@ -16,25 +17,23 @@ import app.revanced.util.exception
 )
 @Suppress("unused")
 object BackgroundPlayPatch : BytecodePatch(
-    setOf(BackgroundPlaybackParentFingerprint)
+    setOf(
+        KidsMinimizedPlaybackPolicyControllerFingerprint,
+        BackgroundPlaybackParentFingerprint,
+    ),
 ) {
     override fun execute(context: BytecodeContext) {
+        KidsMinimizedPlaybackPolicyControllerFingerprint.result?.mutableMethod?.addInstruction(
+            0,
+            "return-void",
+        ) ?: throw KidsMinimizedPlaybackPolicyControllerFingerprint.exception
 
-        BackgroundPlaybackParentFingerprint.result?.let {
-            with(
-                context
-                    .toMethodWalker(it.method)
-                    .nextMethod(it.scanResult.patternScanResult!!.startIndex, true)
-                    .getMethod() as MutableMethod
-            ) {
-                addInstructions(
-                    0, """
-                        const/4 v0, 0x1
-                        return v0
-                        """
-                )
-            }
-        } ?: throw BackgroundPlaybackParentFingerprint.exception
-
+        BackgroundPlaybackParentFingerprint.result?.mutableMethod?.addInstructions(
+            0,
+            """
+                const/4 v0, 0x1
+                return v0
+            """,
+        ) ?: throw BackgroundPlaybackParentFingerprint.exception
     }
 }
