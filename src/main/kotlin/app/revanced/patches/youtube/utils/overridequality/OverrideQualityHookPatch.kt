1ee2743c452795dba4c7a52a7779e35568fe3e6a
Aaron Veil <70171475+anddea@users.noreply.github.com>
2024-04-20 12:42:10 +0300

    fix(YouTube - Default video quality): Match original ReVanced code

--- src/main/kotlin/app/revanced/patches/youtube/utils/overridequality/OverrideQualityHookPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/utils/overridequality/OverrideQualityHookPatch.kt
@@ -109,7 +109,7 @@ object OverrideQualityHookPatch : BytecodePatch(
     }
 
     private const val INTEGRATIONS_VIDEO_QUALITY_CLASS_DESCRIPTOR =
-        "$VIDEO_PATH/VideoQualityPatch;"
+        "$VIDEO_PATH/RememberVideoQualityPatch;"
 
     private const val INTEGRATIONS_VIDEO_HELPER_CLASS_DESCRIPTOR =
         "$INTEGRATIONS_PATH/utils/VideoHelpers;"
