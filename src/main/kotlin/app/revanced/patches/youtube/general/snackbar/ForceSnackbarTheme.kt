eafb7b222c2a39ff2a301a3975838e3e675529a8
Aaron Veil <70171475+anddea@users.noreply.github.com>
2024-05-06 15:08:59 +0300

    feat(YouTube - Force snackbar theme): Add options for corner radius, background and stroke colors

--- src/main/kotlin/app/revanced/patches/youtube/general/snackbar/ForceSnackbarTheme.kt
+++ src/main/kotlin/app/revanced/patches/youtube/general/snackbar/ForceSnackbarTheme.kt
@@ -4,8 +4,10 @@ import app.revanced.patcher.data.ResourceContext
 import app.revanced.patcher.patch.ResourcePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.patcher.patch.options.PatchOption.PatchExtensions.stringPatchOption
 import app.revanced.patches.youtube.utils.settings.SettingsPatch
 import app.revanced.util.doRecursively
+import app.revanced.util.insertNode
 import org.w3c.dom.Element
 
 @Patch(
@@ -61,20 +63,92 @@ import org.w3c.dom.Element
 )
 @Suppress("unused")
 object ForceSnackbarTheme : ResourcePatch() {
+    private const val BACKGROUND = "?ytChipBackground"
+    private const val STROKE = "none"
+
+    private val CornerRadius by stringPatchOption(
+        key = "CornerRadius",
+        default = "8.0dip",
+        title = "Corner radius",
+        description = "Specify a corner radius for the snackbar."
+    )
+
+    private val BackgroundColor by stringPatchOption(
+        key = "BackgroundColor",
+        default = BACKGROUND,
+        values = mapOf(
+            "Chip" to BACKGROUND,
+            "Base" to "?ytBaseBackground"
+        ),
+        title = "Background color",
+        description = "Specify a background color for the snackbar."
+    )
+
+    private val StrokeColor by stringPatchOption(
+        key = "StrokeColor",
+        default = STROKE,
+        values = mapOf(
+            "None" to STROKE,
+            "Accent" to "?attr/colorAccent",
+            "Inverted" to "?attr/ytInvertedBackground"
+        ),
+        title = "Stroke color",
+        description = "Specify a stroke color for the snackbar."
+    )
+
     override fun execute(context: ResourceContext) {
 
-        fun editXml(xmlPath: String, tagName: String, attribute: String, value: String) {
+        fun setAttributes(node: Element, vararg attributesAndValues: String?) {
+            for (i in attributesAndValues.indices step 2) {
+                val attribute = attributesAndValues[i]
+                val value = attributesAndValues[i + 1]
+                if (attribute != null && value != null) {
+                    node.setAttribute(attribute, value)
+                }
+            }
+        }
+
+        fun editXml(xmlPath: String, tagName: String, vararg attributesAndValues: String?) {
+            require(attributesAndValues.size % 2 == 0) { "Number of attributes and values must be even." }
+
             context.document[xmlPath].use { editor ->
                 editor.doRecursively { node ->
                     if (node is Element && (tagName.isEmpty() || node.tagName == tagName)) {
-                        node.setAttribute(attribute, value)
+                        setAttributes(node, *attributesAndValues)
                     }
                 }
             }
         }
 
-        editXml("res/drawable/snackbar_rounded_corners_background.xml", "solid", "android:color", "?ytChipBackground")
-        editXml("res/layout/inset_youtube_snackbar.xml", "", "yt:messageTextColor", "?ytTextPrimary")
+        fun insert(xmlPath: String, tagName: String, insertTagName: String, vararg attributesAndValues: String?) {
+            require(attributesAndValues.size % 2 == 0) { "Number of attributes and values must be even." }
+
+            context.document[xmlPath].use { editor ->
+                editor.doRecursively { node ->
+                    if (node is Element && node.tagName == insertTagName) {
+                        node.insertNode(tagName, node) {
+                            setAttributes(this, *attributesAndValues)
+                        }
+                    }
+                }
+            }
+        }
+
+        if (StrokeColor?.lowercase() != "none")
+            insert("res/drawable/snackbar_rounded_corners_background.xml", "stroke", "corners", "android:width", "1dp", "android:color", StrokeColor)
+
+        editXml("res/drawable/snackbar_rounded_corners_background.xml", "corners",
+            "android:bottomLeftRadius", CornerRadius,
+            "android:bottomRightRadius", CornerRadius,
+            "android:topLeftRadius", CornerRadius,
+            "android:topRightRadius", CornerRadius
+        )
+
+        editXml("res/drawable/snackbar_rounded_corners_background.xml", "solid", "android:color", BackgroundColor)
+
+        listOf("res/layout/inset_snackbar.xml", "res/layout/inset_youtube_snackbar.xml",
+            "res/layout-sw600dp/inset_snackbar.xml", "res/layout-sw600dp/inset_youtube_snackbar.xml")
+            .forEach { editXml(it, "", "yt:messageTextColor", "?ytTextPrimary") }
 
         SettingsPatch.updatePatchStatus("Force snackbar theme")
 
