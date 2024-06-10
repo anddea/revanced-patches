62b19b8ca17aab73852ef261458a31b27fbf6748
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-06-11 04:15:45 +0900

    feat(YouTube Music): Add support version `7.04.51`

--- src/main/kotlin/app/revanced/patches/music/utils/compatibility/Constants.kt
+++ src/main/kotlin/app/revanced/patches/music/utils/compatibility/Constants.kt
@@ -7,12 +7,11 @@ object Constants {
         Patch.CompatiblePackage(
             "com.google.android.apps.youtube.music",
             setOf(
-                "6.29.58",
-                "6.31.55",
-                "6.33.52",
-                "6.51.53",
-                "7.02.52",
-                "7.03.51",
+                "6.29.58", // Latest version that supports the 'Restore old player layout' setting.
+                "6.33.52", // Latest version with the legacy code of YouTube Music.
+                "6.42.55", // Latest version that supports Android 7.0
+                "6.51.53", // Latest version of YouTube Music 6.xx.xx
+                "7.04.51", // Latest version supported by the RVX patch.
             )
         )
     )
