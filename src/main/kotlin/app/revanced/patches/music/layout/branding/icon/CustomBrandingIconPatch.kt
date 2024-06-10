208d22f912cef89d6b19aeffa2ceb1cc5dc51cec
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-06-11 02:45:13 +0900

    chore: lint code

--- src/main/kotlin/app/revanced/patches/music/layout/branding/icon/CustomBrandingIconPatch.kt
+++ src/main/kotlin/app/revanced/patches/music/layout/branding/icon/CustomBrandingIconPatch.kt
@@ -6,6 +6,7 @@ import app.revanced.patcher.patch.options.PatchOption.PatchExtensions.booleanPat
 import app.revanced.patcher.patch.options.PatchOption.PatchExtensions.stringPatchOption
 import app.revanced.patches.music.utils.compatibility.Constants.COMPATIBLE_PACKAGE
 import app.revanced.util.ResourceGroup
+import app.revanced.util.Utils.trimIndentMultiline
 import app.revanced.util.copyResources
 import app.revanced.util.patch.BaseResourcePatch
 import java.io.File
@@ -15,44 +16,88 @@ import java.nio.file.Files
 object CustomBrandingIconPatch : BaseResourcePatch(
     name = "Custom branding icon YouTube Music",
     description = "Changes the YouTube Music app icon to the icon specified in options.json.",
-    compatiblePackages = COMPATIBLE_PACKAGE
+    compatiblePackages = COMPATIBLE_PACKAGE,
+    use = false,
 ) {
     private const val DEFAULT_ICON_KEY = "Revancify Blue"
 
     private val availableIcon = mapOf(
+        "AFN Blue" to "afn_blue",
+        "AFN Red" to "afn_red",
         "MMT" to "mmt",
         DEFAULT_ICON_KEY to "revancify_blue",
         "Revancify Red" to "revancify_red",
         "Revancify Yellow" to "revancify_yellow",
-        "AFN Blue" to "afn_blue",
-        "AFN Red" to "afn_red",
         "Vanced Black" to "vanced_black",
-        "Vanced Light" to "vanced_light"
+        "Vanced Light" to "vanced_light",
+        "YouTube Music" to "youtube_music"
     )
 
-    private val SplashHeaderIcon by booleanPatchOption(
-        key = "SplashHeaderIcon",
-        default = true,
-        title = "Splash and header icons",
-        description = "Apply custom branding icon to Splash and Header.",
-        required = true
-    )
-
-    private val mipmapIconResourceFileNames = arrayOf(
-        "adaptiveproduct_youtube_music_background_color_108",
-        "adaptiveproduct_youtube_music_foreground_color_108",
-        "ic_launcher_release"
-    ).map { "$it.png" }.toTypedArray()
-
-    private val mipmapDirectories = arrayOf(
+    private val sizeArray = arrayOf(
         "xxxhdpi",
         "xxhdpi",
         "xhdpi",
         "hdpi",
         "mdpi"
-    ).map { "mipmap-$it" }
+    )
 
-    public var AppIcon by stringPatchOption(
+    private val largeSizeArray = arrayOf(
+        "xlarge-hdpi",
+        "xlarge-mdpi",
+        "large-xhdpi",
+        "large-hdpi",
+        "large-mdpi",
+        "xxhdpi",
+        "xhdpi",
+        "hdpi",
+        "mdpi",
+    )
+
+    private val drawableDirectories = sizeArray.map { "drawable-$it" }
+
+    private val largeDrawableDirectories = largeSizeArray.map { "drawable-$it" }
+
+    private val mipmapDirectories = sizeArray.map { "mipmap-$it" }
+
+    private val headerIconResourceFileNames = arrayOf(
+        "action_bar_logo",
+        "logo_music",
+        "ytm_logo"
+    ).map { "$it.png" }.toTypedArray()
+
+    private val launcherIconResourceFileNames = arrayOf(
+        "adaptiveproduct_youtube_music_background_color_108",
+        "adaptiveproduct_youtube_music_foreground_color_108",
+        "ic_launcher_release"
+    ).map { "$it.png" }.toTypedArray()
+
+    private val splashIconResourceFileNames = arrayOf(
+        // This file only exists in [drawable-hdpi]
+        // Since {@code ResourceUtils#copyResources} checks for null values before copying,
+        // Just adds it to the array.
+        "action_bar_logo_release",
+        "record"
+    ).map { "$it.png" }.toTypedArray()
+
+    private val headerIconResourceGroups = drawableDirectories.map { directory ->
+        ResourceGroup(
+            directory, *headerIconResourceFileNames
+        )
+    }
+
+    private val launcherIconResourceGroups = mipmapDirectories.map { directory ->
+        ResourceGroup(
+            directory, *launcherIconResourceFileNames
+        )
+    }
+
+    private val splashIconResourceGroups = largeDrawableDirectories.map { directory ->
+        ResourceGroup(
+            directory, *splashIconResourceFileNames
+        )
+    }
+
+    private val AppIcon by stringPatchOption(
         key = "AppIcon",
         default = DEFAULT_ICON_KEY,
         values = availableIcon,
@@ -66,22 +111,32 @@ object CustomBrandingIconPatch : BaseResourcePatch(
 
             Each of these folders must contain the following files:
 
-            ${mipmapIconResourceFileNames.joinToString("\n") { "- $it" }}
-            """
-            .split("\n")
-            .joinToString("\n") { it.trimIndent() } // Remove the leading whitespace from each line.
-            .trimIndent(), // Remove the leading newline.
+            ${launcherIconResourceFileNames.joinToString("\n") { "- $it" }}
+            """.trimIndentMultiline(),
+    )
+
+    private val ChangeHeader by booleanPatchOption(
+        key = "ChangeHeader",
+        default = false,
+        title = "Change header",
+        description = "Apply the custom branding icon to the header."
+    )
+
+    private val ChangeSplashIcon by booleanPatchOption(
+        key = "ChangeSplashIcon",
+        default = true,
+        title = "Change splash icons",
+        description = "Apply the custom branding icon to the splash screen."
     )
 
     override fun execute(context: ResourceContext) {
         AppIcon?.let { appIcon ->
             val appIconValue = appIcon.lowercase().replace(" ", "_")
+            val appIconResourcePath = "music/branding/$appIconValue"
+
+            // Check if a custom path is used in the patch options.
             if (!availableIcon.containsValue(appIconValue)) {
-                mipmapDirectories.map { directory ->
-                    ResourceGroup(
-                        directory, *mipmapIconResourceFileNames
-                    )
-                }.let { resourceGroups ->
+                launcherIconResourceGroups.let { resourceGroups ->
                     try {
                         val path = File(appIcon)
                         val resourceDirectory = context["res"]
@@ -98,107 +153,44 @@ object CustomBrandingIconPatch : BaseResourcePatch(
                             }
                         }
                     } catch (_: Exception) {
+                        // Exception is thrown if an invalid path is used in the patch option.
                         throw PatchException("Invalid app icon path: $appIcon")
                     }
                 }
             } else {
-                val resourcePath = "music/branding/$appIconValue"
 
-                // change launcher icon.
-                mipmapDirectories.map { directory ->
-                    ResourceGroup(
-                        directory, *mipmapIconResourceFileNames
-                    )
-                }.let { resourceGroups ->
+                // Change launcher icon.
+                launcherIconResourceGroups.let { resourceGroups ->
                     resourceGroups.forEach {
-                        context.copyResources("$resourcePath/launcher", it)
+                        context.copyResources("$appIconResourcePath/launcher", it)
                     }
                 }
 
-                // change monochrome icon.
+                // Change monochrome icon.
                 arrayOf(
                     ResourceGroup(
                         "drawable",
                         "ic_app_icons_themed_youtube_music.xml"
                     )
                 ).forEach { resourceGroup ->
-                    context.copyResources("$resourcePath/monochrome", resourceGroup)
+                    context.copyResources("$appIconResourcePath/monochrome", resourceGroup)
                 }
 
-                // change resource icons.
-                if (SplashHeaderIcon == true) {
-                    try {
-                        arrayOf(
-                            ResourceGroup(
-                                "drawable-hdpi",
-                                "action_bar_logo_release.png",
-                                "action_bar_logo.png",
-                                "logo_music.png", // 6.32 and earlier
-                                "ytm_logo.png", // 6.33 and later
-                                "record.png",
-                            ),
-
-                            ResourceGroup(
-                                "drawable-large-hdpi",
-                                "record.png",
-                            ),
-
-                            ResourceGroup(
-                                "drawable-large-mdpi",
-                                "record.png",
-                            ),
-
-                            ResourceGroup(
-                                "drawable-large-xhdpi",
-                                "record.png",
-                            ),
-
-                            ResourceGroup(
-                                "drawable-xlarge-hdpi",
-                                "record.png",
-                            ),
-
-                            ResourceGroup(
-                                "drawable-xlarge-mdpi",
-                                "record.png",
-                            ),
-
-                            ResourceGroup(
-                                "drawable-mdpi",
-                                "action_bar_logo.png",
-                                "logo_music.png", // 6.32 and earlier
-                                "ytm_logo.png", // 6.33 and later
-                                "record.png",
-                            ),
-
-                            ResourceGroup(
-                                "drawable-xhdpi",
-                                "action_bar_logo.png",
-                                "logo_music.png", // 6.32 and earlier
-                                "ytm_logo.png", // 6.33 and later
-                                "record.png",
-                            ),
-
-                            ResourceGroup(
-                                "drawable-xxhdpi",
-                                "action_bar_logo.png",
-                                "logo_music.png", // 6.32 and earlier
-                                "ytm_logo.png", // 6.33 and later
-                                "record.png",
-                            ),
-
-
-                            ResourceGroup(
-                                "drawable-xxxhdpi",
-                                "action_bar_logo.png",
-                                "logo_music.png", // 6.32 and earlier
-                                "ytm_logo.png", // 6.33 and later
-                            ),
-                        ).forEach { resourceGroup ->
-                            context.copyResources("$resourcePath/resource", resourceGroup)
+                // Change header.
+                if (ChangeHeader == true) {
+                    headerIconResourceGroups.let { resourceGroups ->
+                        resourceGroups.forEach {
+                            context.copyResources("$appIconResourcePath/header", it)
                         }
-                    } catch (e: Exception) {
-                        // Do nothing
+                    }
+                }
+
+                // Change splash icon.
+                if (ChangeSplashIcon == true) {
+                    splashIconResourceGroups.let { resourceGroups ->
+                        resourceGroups.forEach {
+                            context.copyResources("$appIconResourcePath/splash", it)
+                        }
                     }
                 }
             }
