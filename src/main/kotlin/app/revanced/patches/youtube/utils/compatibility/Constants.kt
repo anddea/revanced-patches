45b5c5dcb5cf7c6880ee4872a994373640e5b637
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-06-11 04:14:53 +0900

    feat(YouTube): Add support version `19.20.35`

--- src/main/kotlin/app/revanced/patches/youtube/utils/compatibility/Constants.kt
+++ src/main/kotlin/app/revanced/patches/youtube/utils/compatibility/Constants.kt
@@ -7,14 +7,13 @@ object Constants {
         Patch.CompatiblePackage(
             "com.google.android.youtube",
             setOf(
-                "18.29.38",
-                "18.33.40",
-                "18.38.44",
-                "18.48.39",
-                "19.05.36",
-                "19.16.39",
-                "19.19.39",
-                "19.20.34",
+                "18.29.38", // Latest version that supports the 'Zoomed to fill' setting.
+                "18.33.40", // Latest version that do not use litho components in Shorts.
+                "18.38.44", // Latest version with no delay in applying video quality on the server side.
+                "18.48.39", // Latest version that do not use Rolling Number.
+                "19.05.36", // Latest version with the least YouTube experimental flag.
+                "19.16.39", // Latest version that supports the 'Restore old seekbar thumbnails' setting.
+                "19.20.35", // Latest version supported by the RVX patch.
             )
         )
     )
