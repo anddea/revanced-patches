5d51d815a7d4948f17ee4133d818492cb406f61d
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-06-11 23:48:56 +0900

    feat(Reddit/Settings for Reddit): add patch option `RVX settings menu name`

--- src/main/kotlin/app/revanced/patches/reddit/utils/settings/SettingsPatch.kt
+++ src/main/kotlin/app/revanced/patches/reddit/utils/settings/SettingsPatch.kt
@@ -2,6 +2,7 @@ package app.revanced.patches.reddit.utils.settings
 
 import app.revanced.patcher.data.ResourceContext
 import app.revanced.patcher.patch.PatchException
+import app.revanced.patcher.patch.options.PatchOption.PatchExtensions.stringPatchOption
 import app.revanced.patches.reddit.utils.compatibility.Constants.COMPATIBLE_PACKAGE
 import app.revanced.patches.reddit.utils.integrations.IntegrationsPatch
 import app.revanced.util.patch.BaseResourcePatch
@@ -18,10 +19,25 @@ object SettingsPatch : BaseResourcePatch(
     compatiblePackages = COMPATIBLE_PACKAGE,
     requiresIntegrations = true
 ) {
+    private const val DEFAULT_NAME = "ReVanced Extended"
+
+    private val RVXSettingsMenuName by stringPatchOption(
+        key = "RVXSettingsMenuName",
+        default = DEFAULT_NAME,
+        title = "RVX settings menu name",
+        description = "The name of the RVX settings menu.",
+        required = true
+    )
     override fun execute(context: ResourceContext) {
         /**
          * Replace settings icon and label
          */
+
+        var settingsLabel = DEFAULT_NAME
+
+        if (!RVXSettingsMenuName.isNullOrEmpty())
+            settingsLabel = RVXSettingsMenuName!!
+
         arrayOf("preferences", "preferences_logged_in").forEach { targetXML ->
             val resDirectory = context["res"]
             val targetXml = resDirectory.resolve("xml").resolve("$targetXML.xml").toPath()
@@ -35,7 +51,7 @@ object SettingsPatch : BaseResourcePatch(
                 preference.readText()
                     .replace(
                         "\"@drawable/icon_text_post\" android:title=\"@string/label_acknowledgements\"",
-                        "\"@drawable/icon_beta_planet\" android:title=\"ReVanced Extended\""
+                        "\"@drawable/icon_beta_planet\" android:title=\"$settingsLabel\""
                     )
             )
         }
