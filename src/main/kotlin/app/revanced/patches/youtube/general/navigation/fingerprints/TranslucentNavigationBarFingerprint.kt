e9ff3c9aa61ea0d4090e02e1fe9722d96cee9841
Aaron Veil <70171475+anddea@users.noreply.github.com>
2024-06-23 15:07:14 +0300

    feat(YouTube - Navigation bar components): Add `Enable translucent navigation bar` setting

---
+++ src/main/kotlin/app/revanced/patches/youtube/general/navigation/fingerprints/TranslucentNavigationBarFingerprint.kt
@@ -0,0 +1,7 @@
+package app.revanced.patches.youtube.general.navigation.fingerprints
+
+import app.revanced.util.fingerprint.LiteralValueFingerprint
+
+internal object TranslucentNavigationBarFingerprint : LiteralValueFingerprint(
+    literalSupplier = { 45630927 }
+)
