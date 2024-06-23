a09dd4c47e6bebb50dfa35b7da0b1b76d073d8bc
Aaron Veil <70171475+anddea@users.noreply.github.com>
2024-06-23 14:58:50 +0300

    feat(YouTube Music): Add `Enable Cairo splash animation` patch (YouTube Music 7.06.53+)

---
+++ src/main/kotlin/app/revanced/patches/music/misc/splash/CairoSplashAnimationPatch.kt
@@ -0,0 +1,44 @@
+package app.revanced.patches.music.misc.splash
+
+import app.revanced.patcher.data.BytecodeContext
+import app.revanced.patcher.patch.BytecodePatch
+import app.revanced.patcher.patch.annotation.CompatiblePackage
+import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.patches.music.misc.splash.fingerprints.CairoSplashAnimationConfigFingerprint
+import app.revanced.patches.music.utils.integrations.Constants.MISC_PATH
+import app.revanced.patches.music.utils.settings.CategoryType
+import app.revanced.patches.music.utils.settings.SettingsPatch
+import app.revanced.util.literalInstructionBooleanHook
+
+@Patch(
+    name = "Enable Cairo splash animation",
+    description = "Adds an option to enable Cairo splash animation.",
+    dependencies = [SettingsPatch::class],
+    compatiblePackages = [
+        CompatiblePackage(
+            "com.google.android.apps.youtube.music",
+            [
+                "7.06.53",
+            ]
+        )
+    ]
+)
+@Suppress("unused")
+object CairoSplashAnimationPatch : BytecodePatch(
+    setOf(CairoSplashAnimationConfigFingerprint)
+) {
+    override fun execute(context: BytecodeContext) {
+
+        CairoSplashAnimationConfigFingerprint.literalInstructionBooleanHook(
+            45635386,
+            "$MISC_PATH/CairoSplashAnimationPatch;->enableCairoSplashAnimation()Z"
+        )
+
+        SettingsPatch.addSwitchPreference(
+            CategoryType.MISC,
+            "revanced_enable_cairo_splash_animation",
+            "false"
+        )
+
+    }
+}
