a842d1743d13dac33db8ba0179f18289b4a8a742
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-07-27 00:44:55 +0900

    feat(YouTube Music): Add support version `7.10.52`

--- src/main/kotlin/app/revanced/patches/music/utils/compatibility/Constants.kt
+++ src/main/kotlin/app/revanced/patches/music/utils/compatibility/Constants.kt
@@ -12,7 +12,7 @@ object Constants {
                 "6.42.55", // This is the latest version that supports Android 7.0
                 "6.51.53", // This is the latest version of YouTube Music 6.xx.xx
                 "7.08.54", // This was the latest version that was supported by the previous patch.
-                "7.10.51", // This is the latest version supported by the RVX patch.
+                "7.10.52", // This is the latest version supported by the RVX patch.
             )
         )
     )
