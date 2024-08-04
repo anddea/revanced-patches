f0cefa85a4288bda0e3f7324629f1c2a0bcc7839
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-08-04 13:10:12 +0900

    fix(YouTube - SponsorBlock): Improve create segment manual seek accuracy

---
+++ src/main/kotlin/app/revanced/patches/youtube/video/information/fingerprints/SeekRelativeFingerprint.kt
@@ -0,0 +1,22 @@
+package app.revanced.patches.youtube.video.information.fingerprints
+
+import app.revanced.patcher.extensions.or
+import app.revanced.patcher.fingerprint.MethodFingerprint
+import app.revanced.patches.youtube.utils.fingerprints.VideoEndFingerprint
+import com.android.tools.smali.dexlib2.AccessFlags
+import com.android.tools.smali.dexlib2.Opcode
+
+/**
+ * Resolves using class found in [VideoEndFingerprint].
+ */
+internal object SeekRelativeFingerprint : MethodFingerprint(
+    accessFlags = AccessFlags.PUBLIC or AccessFlags.FINAL,
+    returnType = "Z",
+    parameters = listOf("J", "L"),
+    opcodes = listOf(
+        Opcode.ADD_LONG_2ADDR,
+        Opcode.INVOKE_VIRTUAL,
+        Opcode.MOVE_RESULT,
+        Opcode.RETURN
+    )
+)
