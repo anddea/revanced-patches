4c7538e921d1c6afef7e4eeb19c2ac1bae9b86d5
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-08-31 19:08:06 +0200

    feat(YouTube): Remove `Spoof client` patch

--- src/main/kotlin/app/revanced/patches/youtube/utils/gms/GmsCoreSupportPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/utils/gms/GmsCoreSupportPatch.kt
@@ -3,8 +3,6 @@ package app.revanced.patches.youtube.utils.gms
 import app.revanced.patches.shared.gms.BaseGmsCoreSupportPatch
 import app.revanced.patches.shared.gms.BaseGmsCoreSupportResourcePatch.Companion.ORIGINAL_PACKAGE_NAME_YOUTUBE
 import app.revanced.patches.youtube.utils.compatibility.Constants.COMPATIBLE_PACKAGE
-import app.revanced.patches.youtube.utils.fix.client.SpoofClientPatch
-import app.revanced.patches.youtube.utils.fix.client.SpoofUserAgentPatch
 import app.revanced.patches.youtube.utils.integrations.IntegrationsPatch
 import app.revanced.patches.youtube.utils.mainactivity.fingerprints.MainActivityFingerprint
 import app.revanced.patches.youtube.utils.settings.SettingsPatch
@@ -15,8 +13,6 @@ object GmsCoreSupportPatch : BaseGmsCoreSupportPatch(
     mainActivityOnCreateFingerprint = MainActivityFingerprint,
     integrationsPatchDependency = IntegrationsPatch::class,
     dependencies = setOf(
-        SpoofClientPatch::class,
-        SpoofUserAgentPatch::class,
         SettingsPatch::class
     ),
     gmsCoreSupportResourcePatch = GmsCoreSupportResourcePatch,
