2f9c7a9d63765cdb903000ef1d88dd25c4dd585d
Aaron Veil <70171475+anddea@users.noreply.github.com>
2024-09-30 11:22:23 +0300

    refactor(YouTube - Change start page): Change the actual start page instead of redirecting the Url

--- src/main/kotlin/app/revanced/patches/youtube/general/startpage/fingerprints/IntentActionFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/general/startpage/fingerprints/IntentActionFingerprint.kt
@@ -2,7 +2,7 @@ package app.revanced.patches.youtube.general.startpage.fingerprints
 
 import app.revanced.patcher.fingerprint.MethodFingerprint
 
-internal object ShortcutsActivityFingerprint : MethodFingerprint(
+internal object IntentActionFingerprint : MethodFingerprint(
     parameters = listOf("Landroid/content/Intent;"),
     strings = listOf("has_handled_intent"),
 )
