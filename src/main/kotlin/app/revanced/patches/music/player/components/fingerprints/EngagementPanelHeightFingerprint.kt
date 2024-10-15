db80b1d0dd30d0f19323541c9018ff5ab62dae2c
Aaron Veil <70171475+anddea@users.noreply.github.com>
2024-10-15 13:31:10 +0300

    fix(YouTube Music - Player components): Patches do not apply properly in certain versions

--- src/main/kotlin/app/revanced/patches/music/player/components/fingerprints/EngagementPanelHeightFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/music/player/components/fingerprints/EngagementPanelHeightFingerprint.kt
@@ -11,7 +11,9 @@ import com.android.tools.smali.dexlib2.iface.reference.MethodReference
 internal object EngagementPanelHeightFingerprint : MethodFingerprint(
     returnType = "L",
     accessFlags = AccessFlags.PUBLIC or AccessFlags.FINAL,
+    // In YouTube Music 7.21.50+, there are two methods with similar structure, so this Opcode pattern must be used.
     opcodes = listOf(
+        Opcode.IGET_OBJECT,
         Opcode.IGET_OBJECT,
         Opcode.INVOKE_VIRTUAL,
         Opcode.MOVE_RESULT,
