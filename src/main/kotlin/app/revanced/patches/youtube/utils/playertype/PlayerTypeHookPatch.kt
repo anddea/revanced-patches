ef342ace95a6b935e92545557e7a5d8c9315f81a
Aaron Veil <70171475+anddea@users.noreply.github.com>
2024-10-23 17:17:41 +0300

    chore(YouTube - PlayerType): Supplement PlayerType limits under certain conditions

--- src/main/kotlin/app/revanced/patches/youtube/utils/playertype/PlayerTypeHookPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/utils/playertype/PlayerTypeHookPatch.kt
@@ -7,6 +7,8 @@ import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.PatchException
 import app.revanced.patcher.patch.annotation.Patch
+import app.revanced.patches.shared.litho.LithoFilterPatch
+import app.revanced.patches.youtube.utils.integrations.Constants.COMPONENTS_PATH
 import app.revanced.patches.youtube.utils.integrations.Constants.SHARED_PATH
 import app.revanced.patches.youtube.utils.integrations.Constants.UTILS_PATH
 import app.revanced.patches.youtube.utils.playertype.fingerprint.ActionBarSearchResultsFingerprint
@@ -29,7 +31,12 @@ import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 import com.android.tools.smali.dexlib2.iface.instruction.ReferenceInstruction
 import com.android.tools.smali.dexlib2.iface.reference.FieldReference
 
-@Patch(dependencies = [SharedResourceIdPatch::class])
+@Patch(
+    dependencies = [
+        SharedResourceIdPatch::class,
+        LithoFilterPatch::class
+    ]
+)
 object PlayerTypeHookPatch : BytecodePatch(
     setOf(
         ActionBarSearchResultsFingerprint,
@@ -44,6 +51,9 @@ object PlayerTypeHookPatch : BytecodePatch(
 
     private const val INTEGRATIONS_ROOT_VIEW_HOOK_CLASS_DESCRIPTOR =
         "$SHARED_PATH/RootView;"
+
+    private const val FILTER_CLASS_DESCRIPTOR =
+        "$COMPONENTS_PATH/RelatedVideoFilter;"
 
     override fun execute(context: BytecodeContext) {
 
@@ -152,6 +162,8 @@ object PlayerTypeHookPatch : BytecodePatch(
         }
 
         // endregion
+
+        LithoFilterPatch.addFilter(FILTER_CLASS_DESCRIPTOR)
 
     }
 }
