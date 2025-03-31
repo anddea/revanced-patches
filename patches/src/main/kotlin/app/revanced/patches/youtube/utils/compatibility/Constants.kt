c83ed0be9019059b573e72adebcc7d6baf607edd
Aaron Veil <70171475+anddea@users.noreply.github.com>
2025-03-31 13:55:40 +0300

    feat(YouTube): Remove support version `20.03.43`

--- patches/src/main/kotlin/app/revanced/patches/youtube/utils/compatibility/Constants.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/utils/compatibility/Constants.kt
@@ -13,8 +13,7 @@ internal object Constants {
             "19.16.39", // This is the last version where the 'Restore old seekbar thumbnails' setting works.
             "19.43.41", // This is the latest version where edge-to-edge display is not enforced on Android 15+.
             "19.44.39", // This is the only version that has experimental shortcut icons.
-            "19.47.53", // This was the latest version supported by the previous RVX patch.
-            "20.03.43", // This is the latest version supported by the RVX patch.
+            "19.47.53", // This is the latest version supported by the RVX patch.
         )
     )
 }
