ae93a3a16cf45fb12a239d405df4dbbc7ebe3fa0
Aaron Veil <70171475+anddea@users.noreply.github.com>
2026-02-19 11:59:49 +0300

    chore: Update license headers

--- extensions/shared/src/main/java/app/morphe/extension/youtube/utils/GeminiManager.java
+++ extensions/shared/src/main/java/app/morphe/extension/youtube/utils/GeminiManager.java
@@ -1,14 +1,41 @@
 /*
  * Copyright (C) 2025 anddea
  *
- * This file is part of https://github.com/anddea/revanced-patches/.
+ * This file is part of the revanced-patches project:
+ * https://github.com/anddea/revanced-patches
  *
- * The original author: https://github.com/anddea.
+ * Original author(s):
+ * - anddea (https://github.com/anddea)
  *
- * IMPORTANT: This file is the proprietary work of https://github.com/anddea.
- * Any modifications, derivatives, or substantial rewrites of this file
- * must retain this copyright notice and the original author attribution
- * in the source code and version control history.
+ * Licensed under the GNU General Public License v3.0.
+ *
+ * ------------------------------------------------------------------------
+ * GPLv3 Section 7 – Attribution Notice
+ * ------------------------------------------------------------------------
+ *
+ * This file contains substantial original work by the author(s) listed above.
+ *
+ * In accordance with Section 7 of the GNU General Public License v3.0,
+ * the following additional terms apply to this file:
+ *
+ * 1. Attribution (Section 7(b)): This specific copyright notice and the
+ *    list of original authors above must be preserved in any copy or
+ *    derivative work. You may add your own copyright notice below it,
+ *    but you may not remove the original one.
+ *
+ * 2. Origin (Section 7(c)): Modified versions must be clearly marked as
+ *    such (e.g., by adding a "Modified by" line or a new copyright notice).
+ *    They must not be misrepresented as the original work.
+ *
+ * ------------------------------------------------------------------------
+ * Version Control Acknowledgement (Non-binding Request)
+ * ------------------------------------------------------------------------
+ *
+ * While not a legal requirement of the GPLv3, the original author(s)
+ * respectfully request that ports or substantial modifications retain
+ * historical authorship credit in version control systems (e.g., Git),
+ * listing original author(s) appropriately and modifiers as committers
+ * or co-authors.
  */
 
 package app.morphe.extension.youtube.utils;
--- extensions/shared/src/main/java/app/morphe/extension/youtube/utils/VideoUtils.java
+++ extensions/shared/src/main/java/app/morphe/extension/youtube/utils/VideoUtils.java
@@ -1,3 +1,45 @@
+/*
+ * Copyright (C) anddea
+ *
+ * This file is part of the revanced-patches project:
+ * https://github.com/anddea/revanced-patches
+ *
+ * Original author(s) (alphabetical order):
+ * - anddea (https://github.com/anddea)
+ * - inotia00 (https://github.com/inotia00)
+ * - Jav1x (https://github.com/Jav1x)
+ *
+ * Licensed under the GNU General Public License v3.0.
+ *
+ * ------------------------------------------------------------------------
+ * GPLv3 Section 7(b) – Attribution Notice
+ * ------------------------------------------------------------------------
+ *
+ * This file contains substantial original work by the author(s) listed above.
+ *
+ * In accordance with Section 7 of the GNU General Public License v3.0,
+ * the following additional terms apply to this file:
+ *
+ * 1. Attribution (Section 7(b)): This specific copyright notice and the
+ *    list of original authors above must be preserved in any copy or
+ *    derivative work. You may add your own copyright notice below it,
+ *    but you may not remove the original one.
+ *
+ * 2. Origin (Section 7(c)): Modified versions must be clearly marked as
+ *    such (e.g., by adding a "Modified by" line or a new copyright notice).
+ *    They must not be misrepresented as the original work.
+ *
+ * ------------------------------------------------------------------------
+ * Version Control Acknowledgement (Non-binding Request)
+ * ------------------------------------------------------------------------
+ *
+ * While not a legal requirement of the GPLv3, the original author(s)
+ * respectfully request that ports or substantial modifications retain
+ * historical authorship credit in version control systems (e.g., Git),
+ * listing original author(s) appropriately and modifiers as committers
+ * or co-authors.
+ */
+
 package app.morphe.extension.youtube.utils;
 
 import static app.morphe.extension.shared.utils.BaseThemeUtils.getDialogBackgroundColor;
