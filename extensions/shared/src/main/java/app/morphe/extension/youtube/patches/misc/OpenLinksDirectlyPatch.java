4530e0a4db262fcf2f84addec874a44f9f0ce025
rufusin <10528442+rufusin@users.noreply.github.com>
2026-03-05 10:11:02 +0300

    feat(YouTube): Refactor bypass URL redirects

--- extensions/shared/src/main/java/app/morphe/extension/youtube/patches/misc/OpenLinksDirectlyPatch.java
+++ extensions/shared/src/main/java/app/morphe/extension/youtube/patches/misc/OpenLinksDirectlyPatch.java
@@ -2,21 +2,28 @@ package app.morphe.extension.youtube.patches.misc;
 
 import android.net.Uri;
 
-import java.util.Objects;
-
+import app.morphe.extension.shared.utils.Logger;
 import app.morphe.extension.youtube.settings.Settings;
 
 @SuppressWarnings("unused")
 public class OpenLinksDirectlyPatch {
     private static final String YOUTUBE_REDIRECT_PATH = "/redirect";
 
+    /**
+     * Convert the YouTube redirect URI string to the redirect query URI.
+     *
+     * @param uri The YouTube redirect URI string.
+     * @return The redirect query URI.
+     */
     public static Uri parseRedirectUri(String uri) {
-        final Uri parsed = Uri.parse(uri);
-        if (!Settings.BYPASS_URL_REDIRECTS.get())
-            return parsed;
+        final var parsed = Uri.parse(uri);
 
-        if (Objects.equals(parsed.getPath(), YOUTUBE_REDIRECT_PATH)) {
-            return Uri.parse(Uri.decode(parsed.getQueryParameter("q")));
+        if (Settings.BYPASS_URL_REDIRECTS.get() && parsed.getPath().equals(YOUTUBE_REDIRECT_PATH)) {
+            var query = Uri.parse(Uri.decode(parsed.getQueryParameter("q")));
+
+            Logger.printDebug(() -> "Bypassing YouTube redirect URI: " + query);
+
+            return query;
         }
 
         return parsed;
--- patches/src/main/kotlin/app/morphe/patches/youtube/misc/openlinks/directly/OpenLinksDirectlyPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/youtube/misc/openlinks/directly/OpenLinksDirectlyPatch.kt
@@ -5,43 +5,80 @@ import app.morphe.patcher.extensions.InstructionExtensions.replaceInstruction
 import app.morphe.patcher.patch.bytecodePatch
 import app.morphe.patches.youtube.utils.compatibility.Constants.COMPATIBLE_PACKAGE
 import app.morphe.patches.youtube.utils.extension.Constants.MISC_PATH
+import app.morphe.patches.youtube.utils.extension.sharedExtensionPatch
 import app.morphe.patches.youtube.utils.patch.PatchList.BYPASS_URL_REDIRECTS
+import app.morphe.patches.youtube.utils.playservice.is_20_10_or_greater
+import app.morphe.patches.youtube.utils.playservice.is_20_37_or_greater
+import app.morphe.patches.youtube.utils.playservice.is_20_49_or_greater
 import app.morphe.patches.youtube.utils.settings.ResourceUtils.addPreference
 import app.morphe.patches.youtube.utils.settings.settingsPatch
+import com.android.tools.smali.dexlib2.iface.instruction.FiveRegisterInstruction
+
 import app.morphe.util.fingerprint.methodOrThrow
 import app.morphe.util.getReference
 import app.morphe.util.indexOfFirstInstructionOrThrow
-import com.android.tools.smali.dexlib2.Opcode
-import com.android.tools.smali.dexlib2.iface.instruction.FiveRegisterInstruction
 import com.android.tools.smali.dexlib2.iface.reference.MethodReference
+import com.android.tools.smali.dexlib2.Opcode
 
-@Suppress("unused")
-val openLinksDirectlyPatch = bytecodePatch(
+val bypassURLRedirectsPatch = bytecodePatch(
     BYPASS_URL_REDIRECTS.title,
     BYPASS_URL_REDIRECTS.summary,
 ) {
+    dependsOn(
+        sharedExtensionPatch,
+        settingsPatch,
+    )
+
     compatibleWith(COMPATIBLE_PACKAGE)
 
-    dependsOn(settingsPatch)
-
     execute {
+        if (is_20_10_or_greater) {
+            arrayOf(
+                HttpUriParserFingerprint to 0,
 
-        arrayOf(
-            openLinksDirectlyFingerprintPrimary,
-            openLinksDirectlyFingerprintSecondary
-        ).forEach { fingerprint ->
-            fingerprint.methodOrThrow().apply {
-                val insertIndex = indexOfFirstInstructionOrThrow {
-                    opcode == Opcode.INVOKE_STATIC &&
-                            getReference<MethodReference>()?.name == "parse"
+                if (is_20_49_or_greater) {
+                    // Code has moved, and now seems to be an account URL
+                    // and may not be anything to do with sharing links.
+                    null to -1
+                } else if (is_20_37_or_greater) {
+                    AbUriParserFingerprint to 2
+                } else {
+                    AbUriParserLegacyFingerprint to 2
                 }
-                val insertRegister =
-                    getInstruction<FiveRegisterInstruction>(insertIndex).registerC
+            ).forEach { (fingerprint, index) ->
+                if (fingerprint == null) return@forEach
 
-                replaceInstruction(
-                    insertIndex,
-                    "invoke-static {v$insertRegister}, $MISC_PATH/OpenLinksDirectlyPatch;->parseRedirectUri(Ljava/lang/String;)Landroid/net/Uri;"
-                )
+                fingerprint.method.apply {
+                    val insertIndex = fingerprint.instructionMatches[index].index
+                    val uriStringRegister = getInstruction<FiveRegisterInstruction>(insertIndex).registerC
+
+                    replaceInstruction(
+                        insertIndex,
+                        "invoke-static { v$uriStringRegister }, $MISC_PATH/OpenLinksDirectlyPatch;->" +
+                                "parseRedirectUri(Ljava/lang/String;)Landroid/net/Uri;",
+                    )
+                }
+            }
+        }
+
+        if (!is_20_10_or_greater) {
+            arrayOf(
+                openLinksDirectlyFingerprintPrimary,
+                openLinksDirectlyFingerprintSecondary
+            ).forEach { fingerprint ->
+                fingerprint.methodOrThrow().apply {
+                    val insertIndex = indexOfFirstInstructionOrThrow {
+                        opcode == Opcode.INVOKE_STATIC &&
+                                getReference<MethodReference>()?.name == "parse"
+                    }
+                    val insertRegister =
+                        getInstruction<FiveRegisterInstruction>(insertIndex).registerC
+
+                    replaceInstruction(
+                        insertIndex,
+                        "invoke-static {v$insertRegister}, $MISC_PATH/OpenLinksDirectlyPatch;->parseRedirectUri(Ljava/lang/String;)Landroid/net/Uri;"
+                    )
+                }
             }
         }
 
@@ -55,6 +92,5 @@ val openLinksDirectlyPatch = bytecodePatch(
         )
 
         // endregion
-
     }
 }
