b60e5c86ae3a9972b685d72aac85ce1d3ca629bb
anddea <70171475+anddea@users.noreply.github.com>
2026-07-24 18:30:08 +0300

    fix(YouTube & YouTube Music - Return YouTube Username): Did not work for some versions
    Close https://github.com/anddea/revanced-patches/issues/1654

--- patches/src/main/kotlin/app/morphe/patches/shared/textcomponent/TextComponentPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/shared/textcomponent/TextComponentPatch.kt
@@ -1,44 +1,3 @@
-/*
- * Copyright (C) 2026 anddea
- *
- * This file is part of the revanced-patches project:
- * https://github.com/anddea/revanced-patches
- *
- * Original author(s):
- * - anddea (https://github.com/anddea)
- * - inotia00 (https://github.com/inotia00)
- *
- * Licensed under the GNU General Public License v3.0.
- *
- * ------------------------------------------------------------------------
- * GPLv3 Section 7 – Additional Terms & Attribution Requirements
- * ------------------------------------------------------------------------
- *
- * This file contains substantial original work by the author(s) listed above.
- *
- * In accordance with Section 7 of the GNU General Public License v3.0,
- * the following additional terms apply to this file:
- *
- * 1. Source Credit Preservation (Section 7(b)): This specific copyright notice
- *    and the list of original authors above must be preserved in any copy
- *    or derivative work. You may add your own copyright notice below it,
- *    but you may not remove the original one.
- *
- * 2. Origin & Modification Marking (Section 7(c)): Modified versions must be
- *    clearly marked as such (e.g., by adding a "Modified by" line or a new
- *    copyright notice) and must not be misrepresented as the original work.
- *
- * 3. Version Control Attribution (Section 7(b)): Any ports or substantial
- *    modifications must retain historical authorship credit in version control
- *    systems (e.g., Git), listing original author(s) appropriately and
- *    modifiers as committers or co-authors.
- *
- * 4. User Interface Attribution (Section 7(b)): Any works containing or
- *    derived from this material must maintain a visible credit or
- *    acknowledgment to the original author(s) within the application's
- *    user interface (e.g., in an "About" or "Credits" section).
- */
-
 package app.morphe.patches.shared.textcomponent
 
 import app.morphe.patcher.extensions.InstructionExtensions.addInstruction
@@ -48,16 +7,20 @@ import app.morphe.patcher.extensions.InstructionExtensions.replaceInstruction
 import app.morphe.patcher.patch.PatchException
 import app.morphe.patcher.patch.bytecodePatch
 import app.morphe.patcher.util.proxy.mutableTypes.MutableMethod
+import app.morphe.patches.music.utils.playservice.is_9_00_or_greater
+import app.morphe.patches.music.utils.playservice.versionCheckPatch
 import app.morphe.patches.shared.SPANNABLE_STRING_REFERENCE
 import app.morphe.patches.shared.indexOfSpannableStringInstruction
 import app.morphe.patches.shared.spannableStringBuilderFingerprint
 import app.morphe.util.fingerprint.methodOrThrow
 import app.morphe.util.getReference
 import app.morphe.util.indexOfFirstInstruction
-import app.morphe.util.indexOfFirstInstructionReversedOrThrow
+import app.morphe.util.indexOfFirstInstructionOrThrow
 import com.android.tools.smali.dexlib2.Opcode
 import com.android.tools.smali.dexlib2.iface.instruction.FiveRegisterInstruction
 import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
+import com.android.tools.smali.dexlib2.iface.instruction.ReferenceInstruction
+import com.android.tools.smali.dexlib2.iface.instruction.RegisterRangeInstruction
 import com.android.tools.smali.dexlib2.iface.instruction.TwoRegisterInstruction
 import com.android.tools.smali.dexlib2.iface.reference.FieldReference
 import com.android.tools.smali.dexlib2.iface.reference.MethodReference
@@ -75,6 +38,7 @@ private var textComponentContextRegister = 0
 val textComponentPatch = bytecodePatch(
     description = "textComponentPatch"
 ) {
+    dependsOn(versionCheckPatch)
     execute {
         spannableStringBuilderFingerprint.methodOrThrow().apply {
             spannedMethod = this
@@ -95,41 +59,72 @@ val textComponentPatch = bytecodePatch(
 
         TextComponentContextFingerprint.method.apply {
             textComponentMethod = this
+            if (is_9_00_or_greater) {
+                val charSequenceInvokeIndex = indexOfFirstInstruction {
+                    val reference = getReference<MethodReference>()
+                    (opcode == Opcode.INVOKE_STATIC || opcode == Opcode.INVOKE_STATIC_RANGE) &&
+                            reference?.returnType == "Ljava/lang/CharSequence;"
+                }
 
-            // ~ YouTube 19.32.xx
-            val legacyCharSequenceIndex = indexOfFirstInstruction {
-                getReference<FieldReference>()?.type == "Ljava/util/BitSet;"
-            } - 1
-            val charSequenceIndex = indexOfFirstInstruction {
-                val reference = getReference<MethodReference>()
-                opcode == Opcode.INVOKE_VIRTUAL &&
-                        reference?.returnType == "V" &&
-                        reference.parameterTypes.firstOrNull() == "Ljava/lang/CharSequence;"
-            }
+                if (charSequenceInvokeIndex == -1) {
+                    throw PatchException("Could not find text component CharSequence invocation index")
+                }
 
-            val insertIndex: Int
+                val invokeInstruction = getInstruction(charSequenceInvokeIndex)
+                textComponentContextRegister = when (invokeInstruction) {
+                    is RegisterRangeInstruction -> invokeInstruction.startRegister
+                    is FiveRegisterInstruction -> invokeInstruction.registerC
+                    else -> throw PatchException("Unsupported instruction type for text component context register")
+                }
 
-            if (legacyCharSequenceIndex > -2) {
                 textComponentRegister =
-                    getInstruction<TwoRegisterInstruction>(legacyCharSequenceIndex).registerA
-                insertIndex = legacyCharSequenceIndex - 1
-            } else if (charSequenceIndex > -1) {
-                textComponentRegister =
-                    getInstruction<FiveRegisterInstruction>(charSequenceIndex).registerD
-                insertIndex = charSequenceIndex
+                    getInstruction<OneRegisterInstruction>(charSequenceInvokeIndex + 1).registerA
+
+                textComponentIndex = charSequenceInvokeIndex + 2
             } else {
-                throw PatchException("Could not find insert index")
-            }
+                val conversionContextFieldIndex = indexOfFirstInstructionOrThrow {
+                    getReference<FieldReference>()?.type == "Ljava/util/Map;"
+                } - 1
+                val conversionContextFieldReference =
+                    getInstruction<ReferenceInstruction>(conversionContextFieldIndex).reference
 
-            val conversionContextFieldIndex = indexOfFirstInstructionReversedOrThrow(insertIndex) {
-                getReference<FieldReference>()?.type == "Ljava/util/Map;"
-            }
-            val conversionContextRegister = getInstruction<TwoRegisterInstruction>(
-                conversionContextFieldIndex
-            ).registerA
+                // ~ YouTube 19.32.xx
+                val legacyCharSequenceIndex = indexOfFirstInstruction {
+                    getReference<FieldReference>()?.type == "Ljava/util/BitSet;"
+                } - 1
+                val charSequenceIndex = indexOfFirstInstruction {
+                    val reference = getReference<MethodReference>()
+                    opcode == Opcode.INVOKE_VIRTUAL &&
+                            reference?.returnType == "V" &&
+                            reference.parameterTypes.firstOrNull() == "Ljava/lang/CharSequence;"
+                }
 
-            textComponentContextRegister = conversionContextRegister
-            textComponentIndex = insertIndex
+                val insertIndex: Int
+
+                if (legacyCharSequenceIndex > -2) {
+                    textComponentRegister =
+                        getInstruction<TwoRegisterInstruction>(legacyCharSequenceIndex).registerA
+                    insertIndex = legacyCharSequenceIndex - 1
+                } else if (charSequenceIndex > -1) {
+                    textComponentRegister =
+                        getInstruction<FiveRegisterInstruction>(charSequenceIndex).registerD
+                    insertIndex = charSequenceIndex
+                } else {
+                    throw PatchException("Could not find insert index")
+                }
+
+                textComponentContextRegister = getInstruction<TwoRegisterInstruction>(
+                    indexOfFirstInstructionOrThrow(insertIndex, Opcode.IGET_OBJECT)
+                ).registerA
+
+                addInstructions(
+                    insertIndex, """
+                        move-object/from16 v$textComponentContextRegister, p0
+                        iget-object v$textComponentContextRegister, v$textComponentContextRegister, $conversionContextFieldReference
+                        """
+                )
+                textComponentIndex = insertIndex + 2
+            }
         }
     }
 }
